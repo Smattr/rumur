014b66643214303619506a4071821fb3bb1e866f
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-09-10 11:14:48 -0700

    Basic code generation for integer constdecls.

---
+++ rumur/CodeGenerator.py
@@ -0,0 +1,50 @@
+def generate(n):
+    if n.head == 'constdecl':
+        symbol = mangle(n.tail[0].tail[0])
+        value = hang(generate(n.tail[1]))
+        return 'static mpz_t %(symbol)s;\n' \
+               'static void %(symbol)s_init(void) __attribute__((constructor)) {\n' \
+               '  mpz_t x = %(value)s;\n' \
+               '  mpz_init_set(%(symbol)s, x);\n' \
+               '  mpz_clear(x);\n' \
+               '}' % locals()
+
+    elif n.head == 'decl':
+        return concat(n.tail)
+
+    elif n.head == 'expr':
+        if len(n.tail) == 1:
+            return generate(n.tail[0])
+        raise NotImplementedError
+
+    elif n.head == 'integer_constant':
+        value = n.tail[0]
+        return '({\n' \
+               '  mpz_t x;\n' \
+               '  mpz_init_set_ui(x, %(value)s);\n' \
+               '  x;\n' \
+               '})' % locals()
+
+    elif n.head == 'start':
+        if len(n.tail) != 1:
+            raise Exception('multiple initial tokens')
+        return generate(n.tail[0])
+
+    elif n.head == 'program':
+        return concat(n.tail)
+
+    else:
+        raise NotImplementedError
+
+def concat(xs):
+    return '\n\n'.join(map(generate, xs))
+
+def mangle(symbol):
+    return 'user_%s' % symbol
+
+def hang(block):
+    lines = block.split('\n')
+    indented = lines[0]
+    if len(lines) > 1:
+        indented += '\n%s' % '\n'.join(map(lambda x: '  %s' % x, lines[1:]))
+    return indented
