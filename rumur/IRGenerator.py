d754dbf16c483ce01d97f20c941d37229cb748e8
Matthew Fernandez <matthew.fernandez@gmail.com>
2015-12-30 14:20:33 +1100

    More vague progress?

--- rumur/IRGenerator.py
+++ rumur/IRGenerator.py
@@ -1,5 +1,7 @@
+from ConstantFolding import constant_fold
 from Environment import Environment
-from IR import AliasRule, ConstDecl, Invariant, Method, Program, RuleSet, SimpleRule, StartState, TypeDecl, VarDecl
+from IR import AliasRule, Invariant, Lit, Method, Program, RuleSet, SimpleRule, StartState, TypeDecl, VarDecl
+from RumurError import RumurError
 
 class Generator(object):
 
@@ -8,16 +10,26 @@ class Generator(object):
 
     def to_ir(self, node):
 
-        if node.head == 'program':
+        if node.head == 'constdecl':
+            name = str(node.tail[0])
+            expr = self.to_ir(node.tail[1])
+            # Any constant declaration should be able to be folded immediately.
+            expr = constant_fold(self.env, expr)
+            if not isinstance(expr, Lit):
+                raise RumurError('%s:%d: constant declaration is not constant' %
+                    node.filename, node.lineno)
+            self.env.declare(name, expr)
+            return None
+
+        elif node.head == 'program':
             p = Program()
             for t in node.tail:
                 x = self.to_ir(t)
-                if isinstance(x, (ConstDecl, TypeDecl, VarDecl)):
+                if isinstance(x, (TypeDecl, VarDecl)):
                     p.decls.append(x)
                 elif isinstance(x, Method):
                     p.procs.append(x)
-                else:
-                    assert isinstance(x, (AliasRule, Invariant, RuleSet, SimpleRule, StartState))
+                elif isinstance(x, (AliasRule, Invariant, RuleSet, SimpleRule, StartState)):
                     p.rules.append(x)
             return p
 
