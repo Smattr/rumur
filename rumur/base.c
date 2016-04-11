04847ae0d9ccc7437218dfa28bb7af0c1f504e73
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-11 15:53:02 +1000

    Purge some older MPZ stuff.

--- rumur/base.c
+++ rumur/base.c
@@ -3,67 +3,6 @@
 #include <stdlib.h>
 #include <gmp.h>
 
-/* Some mpz functions that GMP does not seem to provide. */
-void mpz_lor(mpz_t dest, const mpz_t x, const mpz_t y) {
-    if (mpz_cmp_ui(x, 0) == 0) {
-        mpz_set(dest, y);
-    } else {
-        mpz_set(dest, x);
-    }
-}
-
-void mpz_land(mpz_t dest, const mpz_t x, const mpz_t y) {
-    if (mpz_cmp_ui(x, 0) == 0) {
-        mpz_set_ui(dest, 0);
-    } else {
-        mpz_set(dest, y);
-    }
-}
-
-void mpz_imp(mpz_t dest, const mpz_t x, const mpz_t y) {
-    if (mpz_cmp_ui(x, 0) != 0) {
-        mpz_set_ui(dest, 1);
-    } else {
-        mpz_set(dest, y);
-    }
-}
-
-void mpz_lt(mpz_t dest, const mpz_t x, const mpz_t y) {
-    mpz_set_si(dest, mpz_cmp(x, y) < 0);
-}
-
-void mpz_lte(mpz_t dest, const mpz_t x, const mpz_t y) {
-    mpz_set_si(dest, mpz_cmp(x, y) <= 0);
-}
-
-void mpz_gt(mpz_t dest, const mpz_t x, const mpz_t y) {
-    mpz_set_si(dest, mpz_cmp(x, y) > 0);
-}
-
-void mpz_gte(mpz_t dest, const mpz_t x, const mpz_t y) {
-    mpz_set_si(dest, mpz_cmp(x, y) >= 0);
-}
-
-void mpz_eq(mpz_t dest, const mpz_t x, const mpz_t y) {
-    mpz_set_si(dest, mpz_cmp(x, y) == 0);
-}
-
-void mpz_neq(mpz_t dest, const mpz_t x, const mpz_t y) {
-    mpz_set_si(dest, mpz_cmp(x, y) != 0);
-}
-
-typedef struct {
-    mpz_t m;
-} st_t;
-
-static void auto_clear(void *st) {
-    assert(mpz != NULL);
-    st_t *s = st;
-    mpz_clear(s->m);
-}
-
-#define temp_st_t __attribute__((cleanup(auto_clear))) st_t
-
 void *xalloc(size_t size) {
     void *p = malloc(size);
     if (p == NULL) {
@@ -72,41 +11,6 @@ void *xalloc(size_t size) {
     }
     return p;
 }
-
-typedef struct state {
-    mpz_t data;
-    struct state *previous;
-} state_t;
-
-void state_init(state_t *s) {
-    mpz_init(s->data);
-    s->previous = NULL;
-}
-
-void state_clone(state_t *s, const state_t *from) {
-    mpz_init_set(s->data, from->data);
-    s->previous = from->previous;
-}
-
-void state_clear(state_t *s) {
-    mpz_clear(s->data);
-}
-
-state_t *state_alloc(void) {
-    state_t *s = xalloc(sizeof(*s));
-    state_init(s);
-    return s;
-}
-
-void state_free(state_t *s) {
-    state_clear(s);
-    free(s);
-}
-
-typedef struct {
-    mpz_t offset;
-    mpz_t size;
-} field_t;
 
 /* This needs to be a macro so we can return an mpz_t. */
 #define r(root, offset, cardinality) \
