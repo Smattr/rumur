5fa15ab53998bd0f201fa0894eda61b75454af4f
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-11 14:13:15 +1000

    Implement constant folding.

--- rumur/OptCF.py
+++ rumur/OptCF.py
@@ -1,12 +1,73 @@
-class ConstantFolder(object):
+'''
+Optimisation - Constant folding
+'''
 
-    def __init__(self, env):
-        self.env = env
+from IR import Add, And, BoolEq, BoolNEq, Div, GT, GTE, Imp, IntEq, IntNEq, Lit, LT, LTE, Mod, \
+    Mul, Not, Or, Sub, TriCond
+from RumurError import RumurError
 
-    def visit(self, node):
+# XXX: Clagged from IRGenerator
+def lineno(stree):
+    stree.calc_position()
+    return stree.min_line
 
-        return node
+def _fold(e):
 
-def constant_fold(env, ir):
-    cf = ConstantFolder(env)
-    return ir.preorder(cf.visit)
+    if isinstance(e, Add) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left + e.right, e.node)
+
+    if isinstance(e, Sub) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left - e.right, e.node)
+
+    if isinstance(e, Mul) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left * e.right, e.node)
+
+    if isinstance(e, Div) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        if e.right.value == 0:
+            raise RumurError('%d: division by zero' % lineno(e.node))
+        return Lit(e.left // e.right, e.node)
+
+    if isinstance(e, Mod) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        if e.right.value == 0:
+            raise RumurError('%d: modulo by zero' % lineno(e.node))
+        return Lit(e.left % e.right, e.node)
+
+    if isinstance(e, Or) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left or e.right, e.node)
+
+    if isinstance(e, And) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left and e.right, e.node)
+
+    if isinstance(e, Imp) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(not e.left or e.right, e.node)
+
+    if isinstance(e, LT) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left < e.right, e.node)
+
+    if isinstance(e, LTE) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left <= e.right, e.node)
+
+    if isinstance(e, GT) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left > e.right, e.node)
+
+    if isinstance(e, GTE) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left >= e.right, e.node)
+
+    if isinstance(e, (BoolEq, IntEq)) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left == e.right, e.node)
+
+    if isinstance(e, (BoolNEq, IntNEq)) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
+        return Lit(e.left != e.right, e.node)
+
+    if isinstance(e, Not) and isinstance(e.operand, Lit):
+        return Lit(not e.operand, e.node)
+
+    if isinstance(e, TriCond) and isinstance(e.cond, Lit):
+        if e.cond.value:
+            return e.casetrue
+        return e.casefalse
+
+    return expr
+
+def constant_fold(ast):
+    return ast.postorder(_fold)
