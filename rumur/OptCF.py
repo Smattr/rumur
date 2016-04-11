0cf692baae9a2fe35b48784b595f272c37c86239
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-11 14:52:33 +1000

    Take advantage of some arithmetic rules in constant folding.

--- rumur/OptCF.py
+++ rumur/OptCF.py
@@ -16,27 +16,62 @@ def _fold(e):
     if isinstance(e, Add) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
         return Lit(e.left + e.right, e.node)
 
+    if isinstance(e, Add) and isinstance(e.left, Lit) and e.left.value == 0:
+        return Lit(e.right, e.node)
+
+    if isinstance(e, Add) and isinstance(e.right, Lit) and e.right.value == 0:
+        return Lit(e.left, e.node)
+
     if isinstance(e, Sub) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
         return Lit(e.left - e.right, e.node)
 
+    if isinstance(e, Sub) and isinstance(e.right, Lit) and e.right.value == 0:
+        return Lit(e.left, e.node)
+
     if isinstance(e, Mul) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
         return Lit(e.left * e.right, e.node)
+
+    if isinstance(e, Mul) and isinstance(e.left, Lit) and e.left.value == 1:
+        return Lit(e.right, e.node)
+
+    if isinstance(e, Mul) and isinstance(e.right, Lit) and e.right.value == 1:
+        return Lit(e.left, e.node)
 
     if isinstance(e, Div) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
         if e.right.value == 0:
             raise RumurError('%d: division by zero' % lineno(e.node))
         return Lit(e.left // e.right, e.node)
 
+    if isinstance(e, Div) and isinstance(e.right, Lit) and e.right.value == 1:
+        return Lit(e.left, e.node)
+
     if isinstance(e, Mod) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
         if e.right.value == 0:
             raise RumurError('%d: modulo by zero' % lineno(e.node))
         return Lit(e.left % e.right, e.node)
 
-    if isinstance(e, Or) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
-        return Lit(e.left or e.right, e.node)
+    if isinstance(e, Mod) and isinstance(e.right, Lit) and e.right.value == 1:
+        return Lit(0, e.node)
 
-    if isinstance(e, And) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
-        return Lit(e.left and e.right, e.node)
+    if isinstance(e, Or) and isinstance(e.left, Lit):
+        if e.left.value:
+            return Lit(True, e.node)
+        return e.right
+
+    if isinstance(e, Or) and isinstance(e.right, Lit):
+        if e.right.value:
+            return Lit(True, e.node)
+        return e.left
+
+    if isinstance(e, And) and isinstance(e.left, Lit):
+        if e.left.value:
+            return e.right
+        return Lit(False, e.node)
+
+    if isinstance(e, And) and isinstance(e.right, Lit):
+        if e.right.value:
+            return e.left
+        return Lit(False, e.node)
 
     if isinstance(e, LT) and isinstance(e.left, Lit) and isinstance(e.right, Lit):
         return Lit(e.left < e.right, e.node)
