f362873186867bb938de59b9da309ae91bf30819
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-07-30 21:20:39 -0700

    wire parsing up to the main functionwire parsing up to the main functionwire parsing up to the main functionwire parsing up to the main functionwire parsing up to the main functionwire parsing up to the main functionwire parsing up to the main functionwire parsing up to the main function

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,19 @@
+#include <iostream>
+#include "parser.yy.hh"
+#include "Model.h"
+#include "Scanner.h"
+#include <unistd.h>
+
+using namespace rumur;
+using namespace std;
+
 int main(void) {
-    return 0;
+
+    Scanner s(&cin);
+    Model *m = nullptr;
+    parser p(s, m);
+
+    int err = p.parse();
+
+    return err == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
