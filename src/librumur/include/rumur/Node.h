6bf7d5d5796ba0f12b146f7a66adbebf2374de53
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-08-01 22:16:45 -0700

    introduce a parent class for location tracking

---
+++ src/librumur/include/rumur/Node.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "location.hh"
+
+namespace rumur {
+
+class Node {
+
+  public:
+    location loc;
+
+    explicit Node(const location &loc) noexcept;
+
+};
+
+}
---
+++ src/librumur/src/Node.cc
@@ -0,0 +1,7 @@
+#include <rumur/Node.h>
+
+using namespace rumur;
+
+Node::Node(const location &loc) noexcept
+  : loc(loc) {
+}
