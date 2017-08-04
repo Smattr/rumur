5adb41d6948ee23c314b794da2803fd6cc49e125
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-08-03 21:34:15 -0700

    rumur: introduce command line arguments

--- src/rumur/src/main.cc
+++ src/rumur/src/main.cc
@@ -1,17 +1,79 @@
 #include <cassert>
+#include <fstream>
+#include <getopt.h>
 #include <iostream>
 #include <rumur/rumur.h>
+#include <string>
 #include <unistd.h>
 
 using namespace rumur;
 using namespace std;
 
-int main(void) {
+static istream *in;
+static string *out;
 
-    scanner s(&cin);
+static void parse_args(int argc, char **argv) {
+
+    for (;;) {
+        static struct option options[] = {
+            { "help", no_argument, 0, '?' },
+            { "output", required_argument, 0, 'o' },
+            { 0, 0, 0, 0 },
+        };
+
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "o:?", options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+
+            case 'o':
+                if (out != nullptr)
+                    delete out;
+                out = new string(optarg);
+                break;
+
+            case '?':
+                cerr << "usage: " << argv[0] << " --output DIR [FILE]\n";
+                exit(EXIT_FAILURE);
+
+            default:
+                cerr << "unexpected error\n";
+                exit(EXIT_FAILURE);
+
+        }
+    }
+
+    if (optind == argc - 1) {
+        ifstream *inf = new ifstream(argv[optind]);
+        if (!inf->is_open()) {
+            cerr << "failed to open " << argv[optind] << "\n";
+            exit(EXIT_FAILURE);
+        }
+        in = inf;
+    } else {
+        in = &cin;
+    }
+
+    if (out == nullptr) {
+        cerr << "output directory is required\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv) {
+
+    // Parse command line options
+    parse_args(argc, argv);
+
+    // Setup the parser
+    scanner s(in);
     Model *m = nullptr;
     parser p(s, m);
 
+    // Parse the input model
     int err;
     try {
         err = p.parse();
@@ -22,6 +84,9 @@ int main(void) {
     if (err != 0)
         return EXIT_FAILURE;
 
+    /* Validate that the model makes sense. E.g. constant definitions are
+     * actually constant.
+     */
     assert(m != nullptr);
     try {
         m->validate();
