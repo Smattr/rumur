e64fa62d570794f2c3c4d4623ea7ea6ef4dfd974
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-09-06 21:49:42 -0700

    librumur: partial implementation of assignment code generation
    Note that this calls out to a generated type function (the writer) for which we
    do not yet generate a definition. So far this looks like a pretty reasonable
    approach though.

--- src/librumur/src/Stmt.cc
+++ src/librumur/src/Stmt.cc
@@ -1,8 +1,11 @@
+#include <cassert>
 #include <iostream>
 #include <memory>
+#include <rumur/except.h>
 #include <rumur/Expr.h>
 #include <rumur/Indexer.h>
 #include <rumur/Stmt.h>
+#include <rumur/TypeExpr.h>
 #include <string>
 
 using namespace rumur;
@@ -15,9 +18,20 @@ Assignment::Assignment(shared_ptr<Lvalue> lhs, shared_ptr<Expr> rhs,
 
 void Assignment::validate() const {
     lhs->validate();
+    if (dynamic_cast<const SimpleTypeExpr*>(lhs->type()) == nullptr)
+        throw RumurError("left hand side of assignment does not have a simple "
+          "type", lhs->loc);
     rhs->validate();
 }
 
-void Assignment::generate_stmt(ostream &out, const string &indent) const {
-    out << indent << "/* TODO */ = /* TODO */;";
+void Assignment::generate_stmt(ostream &out, const string&) const {
+    auto t = dynamic_cast<const SimpleTypeExpr*>(lhs->type());
+    assert (t != nullptr && "BUG: Left hand side of assignment with non-simple "
+      "type. Attempted code generation on unvalidated AST?");
+    t->writer(out);
+    out << "(";
+    lhs->lvalue(out);
+    out << ",";
+    rhs->rvalue(out);
+    out << ");";
 }
