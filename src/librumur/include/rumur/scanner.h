a06215fdd93a396b5cf083e846f244fd00d0e64a
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-01-14 15:49:32 -0800

    squash a false positive warning on Clang

--- src/librumur/include/rumur/scanner.h
+++ src/librumur/include/rumur/scanner.h
@@ -15,9 +15,17 @@ class scanner : public yyFlexLexer {
     // Inherit yyFlexLexer's constructor
     using yyFlexLexer::yyFlexLexer;
 
+/* XXX: Clang's -Woverloaded-virtual decides that the following declaration is
+ * possibly a mistake. However, we are deliberately overloading this method with
+ * a different type signature. The cleanest way I've found around it is to
+ * toggle off the warning here.
+ */
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Woverloaded-virtual"
     // Force a new available type signature for yylex
     virtual int yylex(parser::semantic_type *const lval,
         parser::location_type *loc);
+#pragma clang diagnostic pop
 
 };
 
