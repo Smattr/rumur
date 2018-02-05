94f0ddced491c1af99bd5ea6dfb052112725fcfc
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-02-04 19:48:23 -0800

    add a test that exercises accessing a state-hosted array

---
+++ tests/only-range-and-array.m
@@ -0,0 +1,16 @@
+type
+  range_t: 0 .. 1;
+  array_t: array[range_t] of range_t;
+
+var
+  x: array_t;
+
+startstate begin
+   x[0] := 0;
+   x[1] := 0;
+end;
+
+rule begin
+  x[0] := 1 - x[1];
+  x[1] := 1 - x[0];
+end;
