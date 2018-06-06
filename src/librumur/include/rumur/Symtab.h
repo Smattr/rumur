67de6d92d76dc3c01f8d8088f0d7fb969ca93e38
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-06-05 18:37:14 -0700

    fix: stop interpreting identifiers as case-insensitive
    I had misunderstood something while writing this code and thought that both
    keywords and identifiers were case-insensitive in Murphi when in actual fact
    only the former are. This is the first bug fix in a while that came down to
    literally "delete some code".
    
    Closes Github #25 "case sensitive identifiers"

--- src/librumur/include/rumur/Symtab.h
+++ src/librumur/include/rumur/Symtab.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cassert>
-#include <cctype>
 #include "location.hh"
 #include <rumur/except.h>
 #include <rumur/Node.h>
@@ -11,32 +10,10 @@
 
 namespace rumur {
 
-static inline std::string tolower(const std::string &s) {
-  std::string r;
-  for (const char &c : s)
-    r += ::tolower(c);
-  return r;
-}
-
 class Symtab {
 
  private:
-  // Case-insensitive string hash
-  struct hash {
-    std::size_t operator()(const std::string &s) const noexcept {
-      return std::hash<std::string>()(tolower(s));
-    }
-  };
-
-  // Case-insensitive string comparison
-  struct equal_to {
-    bool operator()(const std::string &a, const std::string &b) const {
-      return tolower(a) == tolower(b);
-    }
-  };
-
- private:
-  std::vector<std::unordered_map<std::string, Node*, hash, equal_to>> scope;
+  std::vector<std::unordered_map<std::string, Node*>> scope;
 
  public:
   void open_scope() {
