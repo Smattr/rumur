673a3510755552b1bdf10c0cb5fa2057cf205d8c
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-06-30 14:31:56 -0700

    test undefining of aggregates

---
+++ tests/undefine-aggregate.m
@@ -0,0 +1,23 @@
+-- test that we can 'undefine' an aggregate
+var
+  x: record
+    a: boolean;
+    b: boolean;
+  end;
+  y: boolean;
+
+startstate begin
+  x.a := true;
+  x.b := false;
+  y := true;
+end;
+
+rule y ==> begin
+  x.a := !x.a;
+  x.b := !x.b;
+end;
+
+rule begin
+  undefine x;
+  y := false;
+end;
