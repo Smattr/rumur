85e8296891aecde4ff8692d85a680a3a46c96093
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-07-19 20:05:30 -0700

    add trivial scalarset test

---
+++ tests/scalarset-trivial.m
@@ -0,0 +1,18 @@
+-- There's no utility to the scalarset in this model. It's purely to validate
+-- that a scalarset can exist.
+
+type
+  t: scalarset(2);
+
+var
+  x: t;
+
+ruleset i: t do
+  startstate begin
+    x := i;
+  end;
+end;
+
+rule begin
+  x := x;
+end;
