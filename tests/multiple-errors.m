d65070af376f9535d6fb7ec67b110bf4b6e56ee2
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-08-02 22:22:05 -0700

    add a test for reporting multiple errors

---
+++ tests/multiple-errors.m
@@ -0,0 +1,28 @@
+-- A model with multiple errors to test --max-errors option
+-- TODO: tweak cmd line args to this and regex the output during testing
+
+var
+  x: 0 .. 5;
+
+startstate begin
+  x := 0;
+end;
+
+rule x < 4 ==> begin
+  x := x + 2;
+end;
+
+rule x > 1 ==> begin
+  x := x - 2;
+end;
+
+rule x < 5 ==> begin
+  x := x + 1;
+end;
+
+rule x > 0 ==> begin
+  x := x - 1;
+end;
+
+invariant x != 5;
+invariant x != 4;
