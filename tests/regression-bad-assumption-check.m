ac50ece481836d4e385456d6849c30ae8d231998
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-10-04 17:30:03 -0700

    add a test for the assumption bug just fixed

---
+++ tests/regression-bad-assumption-check.m
@@ -0,0 +1,23 @@
+/* There was a previous bug wherein assumption checks were done on the preceding
+ * state of a rule, rather than on the final state. This model tests that we
+ * have not re-introduced this bug.
+ */
+
+var
+  x: boolean;
+
+startstate
+  x := true;
+end;
+
+rule begin
+  x := !x;
+end;
+
+rule begin
+  x := x;
+end;
+
+-- This assumption should guarantee that the invariant below it never triggers.
+assume x
+invariant x
