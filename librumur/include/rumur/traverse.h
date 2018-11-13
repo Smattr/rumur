9dbc876a888b83dc6c005480cb47687fa2a138a4
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-11-12 17:54:54 -0800

    remove unused TypeTraversal class
    We may as well reduce these traversal classes to only what we need and then
    implement any further ones on-demand.

--- librumur/include/rumur/traverse.h
+++ librumur/include/rumur/traverse.h
@@ -286,72 +286,6 @@ class StmtTraversal : public BaseTraversal {
   void visit_uexpr(UnaryExpr &n);
 };
 
-// Generic base for traversals that only need to act on TypeExprs
-class TypeTraversal : public BaseTraversal {
-
- public:
-  void visit(Add &n) final;
-  void visit(AliasDecl &n) final;
-  void visit(AliasRule &n) final;
-  void visit(AliasStmt &n) final;
-  void visit(And &n) final;
-  void visit(Array &n) override = 0;
-  void visit(Assignment &n) final;
-  void visit(Clear &n) final;
-  void visit(ConstDecl &n) final;
-  void visit(Div &n) final;
-  void visit(Element &n) final;
-  void visit(Enum&) override = 0;
-  void visit(Eq &n) final;
-  void visit(ErrorStmt &n) final;
-  void visit(Exists &n) final;
-  void visit(ExprID &n) final;
-  void visit(Field &n) final;
-  void visit(For &n) final;
-  void visit(Forall &n) final;
-  void visit(Function &n) final;
-  void visit(FunctionCall &n) final;
-  void visit(Geq &n) final;
-  void visit(Gt &n) final;
-  void visit(If &n) final;
-  void visit(IfClause &n) final;
-  void visit(Implication &n) final;
-  void visit(Leq &n) final;
-  void visit(Lt &n) final;
-  void visit(Model &n) final;
-  void visit(Mod &n) final;
-  void visit(Mul &n) final;
-  void visit(Negative &n) final;
-  void visit(Neq &n) final;
-  void visit(Not &n) final;
-  void visit(Number &n) final;
-  void visit(Or &n) final;
-  void visit(ProcedureCall &n) final;
-  void visit(Property &n) final;
-  void visit(PropertyRule &n) final;
-  void visit(PropertyStmt &n) final;
-  void visit(Quantifier &n) final;
-  void visit(Range &n) override = 0;
-  void visit(Record &n) override = 0;
-  void visit(Return &n) final;
-  void visit(Ruleset &n) final;
-  void visit(Scalarset &n) override = 0;
-  void visit(SimpleRule &n) final;
-  void visit(StartState &n) final;
-  void visit(Sub &n) final;
-  void visit(Ternary &n) final;
-  void visit(TypeDecl &n) final;
-  void visit(TypeExprID &n) override = 0;
-  void visit(Undefine &n) final;
-  void visit(VarDecl &n) final;
-
-  virtual ~TypeTraversal() { }
-
- private:
-  void visit_bexpr(BinaryExpr &n);
-  void visit_uexpr(UnaryExpr &n);
-};
-
 // Read-only equivalent of BaseTraversal.
 class ConstBaseTraversal {
 
@@ -612,7 +546,7 @@ class ConstStmtTraversal : public ConstBaseTraversal {
   void visit_uexpr(const UnaryExpr &n);
 };
 
-// Read-only equivalent ot TypeTraversal
+// Generic base for read-only traversals that only need to act on TypeExprs
 class ConstTypeTraversal : public ConstBaseTraversal {
 
  public:
