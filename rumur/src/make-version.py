1e213bc4314e55f1167c1f6a761dc13ee7e100b3
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-12-08 09:28:10 -0800

    abstract some of the version logic into functions

--- rumur/src/make-version.py
+++ rumur/src/make-version.py
@@ -6,42 +6,38 @@ Generate contents of a version.cc.
 
 import os, re, subprocess, sys
 
-def main(args):
-
-  if len(args) != 2 or args[1] == '--help':
-    sys.stderr.write(
-      'usage: {} file\n'
-      ' write version information as a C++ source file\n'.format(args[0]))
-    return -1
-
-  old = None
-
-  if os.path.exists(args[1]):
-    with open(args[1], 'rt') as f:
-      old = f.read()
-
-  # First, look for a version tag on the current commit.
+def get_tag():
+  '''
+  Find the version tag of the current Git commit, e.g. v2020.05.03, if it
+  exists.
+  '''
   try:
     with open(os.devnull, 'wt') as f:
       tag = subprocess.check_output(['git', 'describe', '--tags'], stderr=f)
   except subprocess.CalledProcessError:
     tag = None
+
   if tag is not None:
     tag = tag.decode('utf-8', 'replace').strip()
     if re.match(r'v[\d\.]+$', tag) is None:
       # Not a version tag.
       tag = None
 
-  if tag is None:
-    # We didn't find a version tag. Use the commit hash as the version.
-    rev = subprocess.check_output(['git', 'rev-parse', '--verify', 'HEAD'])
-    rev = rev.decode('utf-8', 'replace').strip()
+  return tag
 
-    version = 'Git commit {}'.format(rev)
+def get_sha():
+  '''
+  Find the hash of the current Git commit.
+  '''
+  rev = subprocess.check_output(['git', 'rev-parse', '--verify', 'HEAD'])
+  rev = rev.decode('utf-8', 'replace').strip()
 
-  else:
-    version = tag
+  return rev
 
+def is_dirty():
+  '''
+  Determine whether the current working directory has uncommitted changes.
+  '''
   dirty = False
 
   with open(os.devnull, 'wt') as f:
@@ -55,8 +51,37 @@ def main(args):
     p.communicate()
     dirty |= p.returncode != 0
 
-  new = 'const char *VERSION = "{}{}";\n'.format(version,
-    ' (dirty)' if dirty else '')
+  return dirty
+
+def main(args):
+
+  if len(args) != 2 or args[1] == '--help':
+    sys.stderr.write(
+      'usage: {} file\n'
+      ' write version information as a C++ source file\n'.format(args[0]))
+    return -1
+
+  # Get the contents of the old version file if it exists.
+  old = None
+  if os.path.exists(args[1]):
+    with open(args[1], 'rt') as f:
+      old = f.read()
+
+  version = None
+
+  # First, look for a version tag on the current commit.
+  if version is None:
+    tag = get_tag()
+    if tag is not None:
+      version = '{}{}'.format(tag, ' (dirty)' if is_dirty() else '')
+
+  # If we didn't find a version tag. Use the commit hash as the version.
+  if version is None:
+    rev = get_sha()
+    assert rev is not None
+    version = 'Git commit {}{}'.format(rev, ' (dirty)' if is_dirty() else '')
+
+  new = 'const char *VERSION = "{}";\n'.format(version)
 
   # If the version has changed, update the output. Otherwise we leave the old
   # contents -- and more importantly, the timestamp -- intact.
