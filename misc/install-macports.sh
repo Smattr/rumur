f6c5ca123d58d1620ab9faaa804e03e075b6ca62
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-12-18 19:34:42 -0800

    test macOS with Macports on Travis CI

---
+++ misc/install-macports.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Download and install Macports. For use in CI.
+
+# Version of Macports to install
+VERSION=2.5.4
+
+if [ "$(uname)" != "Darwin" ]; then
+  printf 'this script is only intended to run on macOS\n'
+  exit 1
+fi
+
+# Exit on error
+set -e
+
+# Echo all commands
+set -x
+
+# Create a temporary space to work in
+TMP=$(mktemp -d)
+cd "${TMP}"
+
+# Download
+curl --retry 3 -O https://distfiles.macports.org/MacPorts/MacPorts-${VERSION}.tar.bz2
+
+# Decompress
+tar xf MacPorts-${VERSION}.tar.bz2
+cd MacPorts-${VERSION}
+
+# Configure and install
+./configure
+make
+sudo make install
