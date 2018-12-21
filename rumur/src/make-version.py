0babbe28cfde78217623265fec46ab97d78ed5ea
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-12-20 21:51:14 -0800

    release v2018.12.20

--- rumur/src/make-version.py
+++ rumur/src/make-version.py
@@ -8,7 +8,7 @@ import os, re, subprocess, sys
 
 # The version of the last tagged release of Rumur. This will be used as the
 # version number if no Git information is available.
-LAST_RELEASE = 'v2018.12.08'
+LAST_RELEASE = 'v2018.12.20'
 
 def has_git():
   '''
