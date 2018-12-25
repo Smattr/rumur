8b0ef6176da022e12c4c04f26fbe00af5fbcd4c7
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-12-24 18:19:43 -0800

    add a script to test Debian packaging

---
+++ misc/test-debian-packaging.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Test the Debian packaging infrastructure (../debian/)
+
+if [[ "$(uname -v)" != *"Debian"* ]]; then
+  printf 'This script is only intended to run on Debian\n' >&2
+  exit 1
+fi
+
+# Move to our parent directory
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
+
+# Exit on error
+set -e
+
+# Echo all commands
+set -x
+
+# Build the package from scratch
+dpkg-buildpackage -us -uc
+
+# Check for consistency errors in the built package
+lintian -i -I --show-overrides
