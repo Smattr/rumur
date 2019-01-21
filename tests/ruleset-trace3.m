d46d01567ac6a05e83056da20625a9beb8a046cc
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-01-21 14:16:14 -0800

    add a test of printing parameter values of startstates in CEX traces

---
+++ tests/ruleset-trace3.m
@@ -0,0 +1,25 @@
+-- checker_exit_code: 1
+-- checker_output: re.compile(r'^Startstate\s+Startstate 0\s*,\s*y\s*:\s*1\s*,\s*z\s*:\s*8\s*,\s*w\s*:\s*-32\s+fired\b', re.MULTILINE)
+
+/* This is a similar test to ruleset-trace.m but using a ruleset-contained
+ * startstate, instead of an ordinary rule.
+ */
+
+type
+  t1: 0 .. 1;
+  t2: 4 .. 10;
+  t3: -44 .. -2
+
+var
+  x: boolean;
+
+ruleset y: t1; z: t2; w: t3 do
+  startstate begin
+    assert y != 1 | z != 8 | w != -32 "failed assertion";
+    x := true;
+  end;
+end;
+
+rule begin
+  x := !x;
+end;
