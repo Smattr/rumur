Smattr/rumur

commit f5589751de2f860c3cca7d681f9710160d3c20a8

implement a value-to-string conversion to avoid PRIVAL
This removes the last assumption of value_t being int64_t. Now (theoretically)
it should be possible to simply typedef value_t to be something other than
int64_t and have everything work. This is part of a longer arc towards allowing
the user (or automatic optimisation) to select a more performant type for
value_t.

Github: related to #23 "Use native scalars of variable size"

rumur/resources/header.c
@@ -42,7 +42,29 @@
  * enough.
  */
 typedef int64_t value_t;
-#define PRIVAL PRId64
+
+struct value_string_buffer {
+  char data[50];
+};
+
+static struct value_string_buffer value_to_string(value_t v) {
+
+  struct value_string_buffer buf;
+
+  _Generic((value_t)1,
+    int8_t:   snprintf(buf.data, sizeof(buf.data), "%" PRId8,  (int8_t)v),
+    int16_t:  snprintf(buf.data, sizeof(buf.data), "%" PRId16, (int16_t)v),
+    int32_t:  snprintf(buf.data, sizeof(buf.data), "%" PRId32, (int32_t)v),
+    int64_t:  snprintf(buf.data, sizeof(buf.data), "%" PRId64, (int64_t)v),
+    uint8_t:  snprintf(buf.data, sizeof(buf.data), "%" PRIu8,  (uint8_t)v),
+    uint16_t: snprintf(buf.data, sizeof(buf.data), "%" PRIu16, (uint16_t)v),
+    uint32_t: snprintf(buf.data, sizeof(buf.data), "%" PRId32, (uint32_t)v),
+    uint64_t: snprintf(buf.data, sizeof(buf.data), "%" PRId64, (uint64_t)v),
+    default:  assert(!"no valid value_string_buffer() implementation")
+  );
+
+  return buf;
+}
 
 #define VALUE_C(x) _Generic((value_t)1,                                        \
   int8_t:   INT8_C(x),                                                         \
@@ -764,8 +786,8 @@ static value_t handle_read_raw(struct handle h) {
     "the maximum width of a simple type in this model");
 
   if (h.width == 0) {
-    trace(TC_HANDLE_READS, "read value %" PRIVAL " from handle { %p, %zu, %zu }",
-      (value_t)0, h.base, h.offset, h.width);
+    trace(TC_HANDLE_READS, "read value 0 from handle { %p, %zu, %zu }",
+      h.base, h.offset, h.width);
     return 0;
   }
 
@@ -839,8 +861,8 @@ static value_t handle_read_raw(struct handle h) {
 
     value_t v = (value_t)low;
 
-    trace(TC_HANDLE_READS, "read value %" PRIVAL " from handle { %p, %zu, %zu }",
-      v, h.base, h.offset, h.width);
+    trace(TC_HANDLE_READS, "read value %s from handle { %p, %zu, %zu }",
+      value_to_string(v).data, h.base, h.offset, h.width);
 
     return v;
   }
@@ -886,8 +908,8 @@ static value_t handle_read_raw(struct handle h) {
 
   value_t v = (value_t)low;
 
-  trace(TC_HANDLE_READS, "read value %" PRIVAL " from handle { %p, %zu, %zu }",
-    v, h.base, h.offset, h.width);
+  trace(TC_HANDLE_READS, "read value %s from handle { %p, %zu, %zu }",
+    value_to_string(v).data, h.base, h.offset, h.width);
 
   return v;
 }
@@ -931,8 +953,8 @@ static void handle_write_raw(struct handle h, value_t value) {
   ASSERT(h.width <= MAX_SIMPLE_WIDTH && "write of a handle that is larger than "
     "the maximum width of a simple type in this model");
 
-  trace(TC_HANDLE_WRITES, "writing value %" PRIVAL " to handle { %p, %zu, %zu }",
-    value, h.base, h.offset, h.width);
+  trace(TC_HANDLE_WRITES, "writing value %s to handle { %p, %zu, %zu }",
+    value_to_string(value).data, h.base, h.offset, h.width);
 
   if (h.width == 0) {
     return;

rumur/src/generate-expr.cc
@@ -345,8 +345,8 @@ class Generator : public ConstExprTraversal {
             << "if (v != 0 && (v + " << lba << " - 1 < " << lb << " || v + "
               << lba << " - 1 > " << ub << ")) { "
             << "error(s, false, \"call to function %s passed an out-of-range "
-              << "value %\" PRIVAL \" to parameter " << (index + 1) << "\", \""
-              << n.name << "\", v + " << lba << " - 1); "
+              << "value %s to parameter " << (index + 1) << "\", \""
+              << n.name << "\", value_to_string(v + " << lba << " - 1).data); "
             << "} "
             << "handle_write_raw(" << handle << ", v == 0 ? v : (v + " << lba
               << " - " << lb << ")); "

rumur/src/generate-model.cc
@@ -575,8 +575,8 @@ void generate_model(std::ostream &out, const Model &m) {
           for (const Quantifier &q : r->quantifiers) {
             out
               << "      escaped_name = xml_escape(\"" + q.name + "\");\n"
-              << "      printf(\"<parameter name=\\\"%s\\\">%\" PRIVAL "
-                "\"</parameter>\", escaped_name, (value_t)((rule_taken - " << base
+              << "      printf(\"<parameter name=\\\"%s\\\">%s</parameter>\", "
+                "escaped_name, value_to_string((value_t)((rule_taken - " << base
                 << ") / (1";
             size_t j = r->quantifiers.size() - 1;
             for (auto it = r->quantifiers.rbegin(); it != r->quantifiers.rend(); it++) {
@@ -585,7 +585,7 @@ void generate_model(std::ostream &out, const Model &m) {
               out << " * " << it->count();
               j--;
             }
-            out << ") % " << q.count() << ") + " << q.lower_bound() << ");\n"
+            out << ") % " << q.count() << ") + " << q.lower_bound() << ").data);\n"
               << "      free(escaped_name);\n";
             i++;
           }
@@ -599,8 +599,8 @@ void generate_model(std::ostream &out, const Model &m) {
         {
           size_t i = 0;
           for (const Quantifier &q : r->quantifiers) {
-            out << "      printf(\", %s: %\" PRIVAL, \"" << q.name
-              << "\", (value_t)((rule_taken - " << base << ") / (1";
+            out << "      printf(\", %s: %s\", \"" << q.name
+              << "\", value_to_string((value_t)((rule_taken - " << base << ") / (1";
             size_t j = r->quantifiers.size() - 1;
             for (auto it = r->quantifiers.rbegin(); it != r->quantifiers.rend(); it++) {
               if (i == j)
@@ -608,7 +608,7 @@ void generate_model(std::ostream &out, const Model &m) {
               out << " * " << it->count();
               j--;
             }
-            out << ") % " << q.count() << ") + " << q.lower_bound() << ");\n";
+            out << ") % " << q.count() << ") + " << q.lower_bound() << ").data);\n";
             i++;
           }
         }
@@ -667,8 +667,8 @@ void generate_model(std::ostream &out, const Model &m) {
           for (const Quantifier &q : r->quantifiers) {
             out
               << "      escaped_name = xml_escape(\"" + q.name + "\");\n"
-              << "      printf(\"<parameter name=\\\"%s\\\">%\" PRIVAL "
-                "\"</parameter>\", escaped_name, (value_t)((rule_taken - " << base
+              << "      printf(\"<parameter name=\\\"%s\\\">%s</parameter>\", "
+                "escaped_name, value_to_string((value_t)((rule_taken - " << base
                 << ") / (1";
             size_t j = r->quantifiers.size() - 1;
             for (auto it = r->quantifiers.rbegin(); it != r->quantifiers.rend(); it++) {
@@ -677,7 +677,7 @@ void generate_model(std::ostream &out, const Model &m) {
               out << " * " << it->count();
               j--;
             }
-            out << ") % " << q.count() << ") + " << q.lower_bound() << ");\n"
+            out << ") % " << q.count() << ") + " << q.lower_bound() << ").data);\n"
               << "      free(escaped_name);\n";
             i++;
           }
@@ -691,8 +691,8 @@ void generate_model(std::ostream &out, const Model &m) {
         {
           size_t i = 0;
           for (const Quantifier &q : r->quantifiers) {
-            out << "      printf(\", %s: %\" PRIVAL, \"" << q.name
-              << "\", (value_t)((rule_taken - " << base << ") / (1";
+            out << "      printf(\", %s: %s\", \"" << q.name
+              << "\", value_to_string((value_t)((rule_taken - " << base << ") / (1";
             size_t j = r->quantifiers.size() - 1;
             for (auto it = r->quantifiers.rbegin(); it != r->quantifiers.rend(); it++) {
               if (i == j)
@@ -700,7 +700,7 @@ void generate_model(std::ostream &out, const Model &m) {
               out << " * " << it->count();
               j--;
             }
-            out << ") % " << q.count() << ") + " << q.lower_bound() << ");\n";
+            out << ") % " << q.count() << ") + " << q.lower_bound() << ").data);\n";
             i++;
           }
         }

rumur/src/generate-print.cc
@@ -154,8 +154,8 @@ class Generator : public ConstTypeTraversal {
       << "    if (v == 0) {\n"
       << "      printf(\"Undefined\");\n"
       << "    } else {\n"
-      << "      printf(\"%\" PRIVAL \"\", decode_value(" << lb << ", "
-        << ub << ", v));\n"
+      << "      printf(\"%s\", value_to_string(decode_value(" << lb << ", "
+        << ub << ", v)).data);\n"
       << "    }\n"
       << "    if (" << support_xml << " && MACHINE_READABLE_OUTPUT) {\n"
       << "      printf(\"\\\"/>\");\n"
@@ -199,7 +199,7 @@ class Generator : public ConstTypeTraversal {
       << "    if (v == 0) {\n"
       << "      printf(\"Undefined\");\n"
       << "    } else {\n"
-      << "      printf(\"%\" PRIVAL \"\", v - 1);\n"
+      << "      printf(\"%s\", value_to_string(v - 1).data);\n"
       << "    }\n"
       << "    if (" << support_xml << " && MACHINE_READABLE_OUTPUT) {\n"
       << "      printf(\"\\\"/>\");\n"

rumur/src/generate-stmt.cc
@@ -275,9 +275,9 @@ class Generator : public ConstStmtTraversal {
       }
     }
 
-    *out << "printf(\"%\" PRIVAL, ";
+    *out << "printf(\"%s\", value_to_string(";
     generate_rvalue(*out, *s.expr);
-    *out << ")";
+    *out << ").data)";
   }
 
   void visit(const Return &s) final {

commit ccf410672326e04230331576a1c76003ad2ab1a3

expose trace() as a macro
f5589751de2f860c3cca7d681f9710160d3c20a8 caused an unexpected performance
regression and debugging this revealed the cause to be related to calls to
trace() being emitted even with tracing disabled. As described in the comment in
this commit, it seems compilers are unwilling to inline calls to varargs
functions or perform interprocedural optimisations through them. This meant that
we would unconditionally incur the overhead of a varargs function call for each
tracing point even with tracing disabled. This was compounded when dynamic
construction of a string was added to some tracing calls.

As a silver lining to this detour, this commit should actually improve
performance beyond the baseline of what we had before the regression, as the
compiler should now be able to elide these calls (and string construction)
altogether.

rumur/resources/header.c
@@ -467,23 +467,34 @@ static char *xml_escape(const char *s) {
  * checker generation with '--trace ...' and is useful for debugging Rumur
  * itself.
  */
-static __attribute__((format(printf, 2, 3))) void trace(
-  enum trace_category_t category, const char *fmt, ...) {
+static __attribute__((format(printf, 1, 2))) void trace(const char *fmt, ...) {
 
-  if (category & TRACES_ENABLED) {
-    va_list ap;
-    va_start(ap, fmt);
+  va_list ap;
+  va_start(ap, fmt);
 
-    print_lock();
+  print_lock();
 
-    (void)fprintf(stderr, "%sTRACE%s:", yellow(), reset());
-    (void)vfprintf(stderr, fmt, ap);
-    (void)fprintf(stderr, "\n");
+  (void)fprintf(stderr, "%sTRACE%s:", yellow(), reset());
+  (void)vfprintf(stderr, fmt, ap);
+  (void)fprintf(stderr, "\n");
 
-    print_unlock();
-    va_end(ap);
-  }
+  print_unlock();
+  va_end(ap);
 }
+
+/* Wrap up trace() as a macro. It looks as if the following could just be
+ * incorporated into trace(). However, present compilers seem unwilling to
+ * inline varargs functions or do interprocedural analysis across a call to one.
+ * As a result, the compiler does not notice when tracing is disabled and a call
+ * to trace() would be a no-op that can be elided. By making the call a macro we
+ * make the category comparison visible to the compiler's optimising passes.
+ */
+#define TRACE(category, args...)                                               \
+  do {                                                                         \
+    if ((category) & TRACES_ENABLED) {                                         \
+      trace(args);                                                             \
+    }                                                                          \
+  } while (0)
 
 /*******************************************************************************
  * Arithmetic wrappers                                                         *
@@ -786,7 +797,7 @@ static value_t handle_read_raw(struct handle h) {
     "the maximum width of a simple type in this model");
 
   if (h.width == 0) {
-    trace(TC_HANDLE_READS, "read value 0 from handle { %p, %zu, %zu }",
+    TRACE(TC_HANDLE_READS, "read value 0 from handle { %p, %zu, %zu }",
       h.base, h.offset, h.width);
     return 0;
   }
@@ -861,7 +872,7 @@ static value_t handle_read_raw(struct handle h) {
 
     value_t v = (value_t)low;
 
-    trace(TC_HANDLE_READS, "read value %s from handle { %p, %zu, %zu }",
+    TRACE(TC_HANDLE_READS, "read value %s from handle { %p, %zu, %zu }",
       value_to_string(v).data, h.base, h.offset, h.width);
 
     return v;
@@ -908,7 +919,7 @@ static value_t handle_read_raw(struct handle h) {
 
   value_t v = (value_t)low;
 
-  trace(TC_HANDLE_READS, "read value %s from handle { %p, %zu, %zu }",
+  TRACE(TC_HANDLE_READS, "read value %s from handle { %p, %zu, %zu }",
     value_to_string(v).data, h.base, h.offset, h.width);
 
   return v;
@@ -953,7 +964,7 @@ static void handle_write_raw(struct handle h, value_t value) {
   ASSERT(h.width <= MAX_SIMPLE_WIDTH && "write of a handle that is larger than "
     "the maximum width of a simple type in this model");
 
-  trace(TC_HANDLE_WRITES, "writing value %s to handle { %p, %zu, %zu }",
+  TRACE(TC_HANDLE_WRITES, "writing value %s to handle { %p, %zu, %zu }",
     value_to_string(value).data, h.base, h.offset, h.width);
 
   if (h.width == 0) {
@@ -1402,7 +1413,7 @@ size_t queue_enqueue(struct state *s, size_t queue_id) {
 
   q[queue_id].count++;
 
-  trace(TC_QUEUE, "enqueued state %p into queue %zu, queue length is now %zu",
+  TRACE(TC_QUEUE, "enqueued state %p into queue %zu, queue length is now %zu",
     s, queue_id, q[queue_id].count);
 
   size_t count = q[queue_id].count;
@@ -1431,7 +1442,7 @@ const struct state *queue_dequeue(size_t *queue_id) {
         q[*queue_id].tail = NULL;
       }
       q[*queue_id].count--;
-      trace(TC_QUEUE, "dequeued state %p from queue %zu, queue length is now "
+      TRACE(TC_QUEUE, "dequeued state %p from queue %zu, queue length is now "
         "%zu", n->s, *queue_id, q[*queue_id].count);
     }
 
@@ -1861,7 +1872,7 @@ static void set_thread_init(void) {
 
 static void set_migrate(void) {
 
-  trace(TC_SET, "assisting in set migration...");
+  TRACE(TC_SET, "assisting in set migration...");
 
   /* Size of a migration chunk. Threads in this function grab a chunk at a time
    * to migrate.
@@ -1922,7 +1933,7 @@ retry:;
 
   if (count == 0) {
 
-    trace(TC_SET, "arrived at rendezvous point as leader");
+    TRACE(TC_SET, "arrived at rendezvous point as leader");
 
     /* At this point, we know no one is still updating the old set's count, so
      * we can migrate its value to the next set.
@@ -1968,13 +1979,13 @@ static void set_expand(void) {
   if (s != NULL) {
     /* Someone else already expanded it. Join them in the migration effort. */
     set_expand_unlock();
-    trace(TC_SET, "attempted expansion failed because another thread got there "
+    TRACE(TC_SET, "attempted expansion failed because another thread got there "
       "first");
     set_migrate();
     return;
   }
 
-  trace(TC_SET, "expanding set from %zu slots to %zu slots...",
+  TRACE(TC_SET, "expanding set from %zu slots to %zu slots...",
     (((size_t)1) << local_seen->size_exponent) / sizeof(slot_t),
     (((size_t)1) << (local_seen->size_exponent + 1)) / sizeof(slot_t));
 
@@ -2013,7 +2024,7 @@ restart:;
         state_to_slot(s), false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
       /* Success */
       *count = __atomic_add_fetch(&local_seen->count, 1, __ATOMIC_SEQ_CST);
-      trace(TC_SET, "added state %p, set size is now %zu", s, *count);
+      TRACE(TC_SET, "added state %p, set size is now %zu", s, *count);
 
       /* The maximum possible size of the seen state set should be constrained
        * by the number of possible states based on how many bits we are using to
@@ -2037,7 +2048,7 @@ restart:;
 
     /* If we find this already in the set, we're done. */
     if (state_eq(s, slot_to_state(c))) {
-      trace(TC_SET, "skipped adding state %p that was already in set", s);
+      TRACE(TC_SET, "skipped adding state %p that was already in set", s);
       return false;
     }
 