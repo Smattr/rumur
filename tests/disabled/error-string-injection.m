ed16566be8a0fa0a61ccfd692773b75ace6604e5
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-03-29 20:40:25 -0700

    add a test case demonstrating the invariant format code problem
    Github: related to #119 "using printf format codes in invariant names causes
    bad printf usage"

---
+++ tests/disabled/error-string-injection.m
@@ -0,0 +1,21 @@
+-- c_flags: ['-std=c11', '-Werror=format']
+
+/* This model checks we have not reintroduced a problem first observed on commit
+ * e1b74738ba73df5352622464be47b69db688c633, wherein invariant names involving
+ * printf format codes would cause bad calls to the function error() to be
+ * generated in the verifier. If this problem has been introduced, the generated
+ * model will fail to compile with the given flags.
+ */
+
+var
+  x: boolean;
+
+startstate begin
+  x := true;
+end;
+
+rule begin
+  x := !x;
+end;
+
+invariant "test string %s injection" x | !x;
