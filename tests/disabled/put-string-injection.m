7c8273c779524e93f0248810b1adc74e658d3bf9
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-04-01 19:40:20 -0700

    add another model demonstrating a format string problem
    Github: related to #119 "using printf format codes in invariant names causes bad
    printf usage"

---
+++ tests/disabled/put-string-injection.m
@@ -0,0 +1,22 @@
+-- c_flags: ['-std=c11', '-Werror=format'] + (['-mcx16'] if X86_64 else [])
+
+/* This model is designed to trigger a problem first observed on
+ * 0f66cb1de6e4eb359fc4aa1ab4d55c8dd951a04d wherein a crafted call to put could
+ * result in the generation of code that would corrupt its own stack. If this
+ * problem has been re-introduced, the generated code for this model will
+ * trigger a compilation error when passing -Werror=format.
+ */
+
+var
+  s: 0 .. 10;
+  a: array[0 .. 10] of boolean;
+
+startstate begin
+  a[0] := true;
+  s := 1;
+end;
+
+rule begin
+  put a[4%s];
+  a[0] := !a[0];
+end;
