f5c8cc54a8a02338a62985aaf2190d7f5fc79ca0
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-04-20 14:45:16 -0700

    switch to using fast versions of ValueTypes
    We may as well adhere to what the compiler thinks is best for speed as this type
    (value_t) is only used for computation on unpackaged values.
    
    Github: related to #23 "Use native scalars of variable size"

--- rumur/src/ValueType.cc
+++ rumur/src/ValueType.cc
@@ -5,14 +5,14 @@
 #include "ValueType.h"
 
 static const std::unordered_map<std::string, ValueType> types = {
-  { "int8_t",   { "int8_t",   "INT8_MIN",      "INT8_MAX",   "INT8_C"   } },
-  { "uint8_t",  { "uint8_t",  "((uint8_t)0)",  "UINT8_MAX",  "UINT8_C"  } },
-  { "int16_t",  { "int16_t",  "INT16_MIN",     "INT16_MAX",  "INT16_C"  } },
-  { "uint16_t", { "uint16_t", "((uint16_t)0)", "UINT16_MAX", "UINT16_C" } },
-  { "int32_t",  { "int32_t",  "INT32_MIN",     "INT32_MAX",  "INT32_C"  } },
-  { "uint32_t", { "uint32_t", "((uint32_t)0)", "UINT32_MAX", "UINT32_C" } },
-  { "int64_t",  { "int64_t",  "INT64_MIN",     "INT64_MAX",  "INT64_C"  } },
-  { "uint64_t", { "uint64_t", "((uint64_t)0)", "UINT64_MAX", "UINT64_C" } },
+  { "int8_t",   { "int_fast8_t",   "INT_FAST8_MIN",  "INT_FAST8_MAX",   "INT8_C"   } },
+  { "uint8_t",  { "uint_fast8_t",  "((uint8_t)0)",   "UINT_FAST8_MAX",  "UINT8_C"  } },
+  { "int16_t",  { "int_fast16_t",  "INT_FAST16_MIN", "INT_FAST16_MAX",  "INT16_C"  } },
+  { "uint16_t", { "uint_fast16_t", "((uint16_t)0)",  "UINT_FAST16_MAX", "UINT16_C" } },
+  { "int32_t",  { "int_fast32_t",  "INT_FAST32_MIN", "INT_FAST32_MAX",  "INT32_C"  } },
+  { "uint32_t", { "uint_fast32_t", "((uint32_t)0)",  "UINT_FAST32_MAX", "UINT32_C" } },
+  { "int64_t",  { "int_fast64_t",  "INT_FAST64_MIN", "INT_FAST64_MAX",  "INT64_C"  } },
+  { "uint64_t", { "uint_fast64_t", "((uint64_t)0)",  "UINT_FAST64_MAX", "UINT64_C" } },
 };
 
 const ValueType &get_value_type(const std::string &name) {
