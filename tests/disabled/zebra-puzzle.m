7d5e4d509644f2fd978439933ac1b9da08360b63
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-04-28 15:51:05 -0700

    optimise zebra puzzle model
    This change effectively pre-assigns people to houses. There was no need to model
    this decision within the exploration of the model itself because nothing is
    known about the people at the time they are assigned to houses, so this still
    leaves the entire problem space open.
    
    This model is still not checkable within 16GB, so it may be an interesting test
    case to use for work on compressed representations.

--- tests/disabled/zebra-puzzle.m
+++ tests/disabled/zebra-puzzle.m
@@ -13,149 +13,131 @@ type
   cigarette: enum { OLDGOLD, CHESTERFIELDS, KOOLS, LUCKYSTRIKE, PARLIAMENTS }
   drink: enum { COFFEE, TEA, MILK, ORANGEJUICE, WATER }
 
-  identity: record
+  house: record
     citizenship: nationality
-    house_position: 0 .. 4
     paint: colour
     pet: animal
     brand: cigarette
     beverage: drink
   end
 
-  id: scalarset(5)
-
 var
-  person: array [id] of identity
+  houses: array [0 .. 4] of house
 
 startstate begin
 end
 
-ruleset i: id do
+ruleset i: 0 .. 4 do
 
-  -- define rules to make arbitrary assignments for each person
+  -- define rules to make arbitrary assignments for each house
 
   ruleset n: nationality do
-    rule isundefined(person[i].citizenship) ==> begin
-      person[i].citizenship := n;
-    end
-  end
-
-  ruleset p: 0 .. 4 do
-    rule isundefined(person[i].house_position) ==> begin
-      person[i].house_position := p;
+    rule isundefined(houses[i].citizenship) ==> begin
+      houses[i].citizenship := n;
     end
   end
 
   ruleset c: colour do
-    rule isundefined(person[i].paint) ==> begin
-      person[i].paint := c;
+    rule isundefined(houses[i].paint) ==> begin
+      houses[i].paint := c;
     end
   end
 
   ruleset a: animal do
-    rule isundefined(person[i].pet) ==> begin
-      person[i].pet := a;
+    rule isundefined(houses[i].pet) ==> begin
+      houses[i].pet := a;
     end
   end
 
   ruleset c: cigarette do
-    rule isundefined(person[i].brand) ==> begin
-      person[i].brand := c;
+    rule isundefined(houses[i].brand) ==> begin
+      houses[i].brand := c;
     end
   end
 
   ruleset d: drink do
-    rule isundefined(person[i].beverage) ==> begin
-      person[i].beverage := d;
+    rule isundefined(houses[i].beverage) ==> begin
+      houses[i].beverage := d;
     end
   end
 
   -- define assumptions to capture the rules of the puzzle
 
   assume "the Englishman lives in the red house"
-    !isundefined(person[i].citizenship) & !isundefined(person[i].paint)
-    & person[i].citizenship = ENGLISHMAN -> person[i].paint = RED
+    !isundefined(houses[i].citizenship) & !isundefined(houses[i].paint)
+    & houses[i].citizenship = ENGLISHMAN -> houses[i].paint = RED
 
   assume "the Spaniard owns the dog"
-    !isundefined(person[i].citizenship) & !isundefined(person[i].pet)
-    & person[i].citizenship = SPANIARD -> person[i].pet = DOG
+    !isundefined(houses[i].citizenship) & !isundefined(houses[i].pet)
+    & houses[i].citizenship = SPANIARD -> houses[i].pet = DOG
 
   assume "coffee is drunk in the green house"
-    !isundefined(person[i].paint) & !isundefined(person[i].beverage)
-    & person[i].beverage = COFFEE -> person[i].paint = GREEN
+    !isundefined(houses[i].paint) & !isundefined(houses[i].beverage)
+    & houses[i].beverage = COFFEE -> houses[i].paint = GREEN
 
   assume "the Ukranian drinks tea"
-    !isundefined(person[i].citizenship) & !isundefined(person[i].beverage)
-    & person[i].citizenship = UKRANIAN -> person[i].beverage = TEA
+    !isundefined(houses[i].citizenship) & !isundefined(houses[i].beverage)
+    & houses[i].citizenship = UKRANIAN -> houses[i].beverage = TEA
 
   assume "the green house is immediately to the right of the ivory house"
-    forall j: id do
-      !isundefined(person[i].paint) & person[i].paint = GREEN
-      & !isundefined(person[i].house_position)
-      & !isundefined(person[j].paint) & person[j].paint = IVORY
-      & !isundefined(person[j].house_position)
-      -> person[i].house_position = person[j].house_position + 1
+    forall j: 0 .. 4 do
+      !isundefined(houses[i].paint) & houses[i].paint = GREEN
+      & !isundefined(houses[j].paint) & houses[j].paint = IVORY
+      -> i = j + 1
     end
 
   assume "the Old Gold smoker owns snails"
-    !isundefined(person[i].brand) & !isundefined(person[i].pet)
-    & person[i].brand = OLDGOLD -> person[i].pet = SNAIL
+    !isundefined(houses[i].brand) & !isundefined(houses[i].pet)
+    & houses[i].brand = OLDGOLD -> houses[i].pet = SNAIL
 
   assume "Kools are smoked in the yellow house"
-    !isundefined(person[i].brand) & !isundefined(person[i].paint)
-    & person[i].brand = KOOLS -> person[i].paint = YELLOW
+    !isundefined(houses[i].brand) & !isundefined(houses[i].paint)
+    & houses[i].brand = KOOLS -> houses[i].paint = YELLOW
 
   assume "milk is drunk in the middle house"
-    !isundefined(person[i].beverage) & !isundefined(person[i].house_position)
-    & person[i].beverage = MILK -> person[i].house_position = 2
+    !isundefined(houses[i].beverage)
+    & houses[i].beverage = MILK -> i = 2
 
   assume "the Norwegian lives in the first house"
-    !isundefined(person[i].citizenship) & !isundefined(person[i].house_position)
-    & person[i].citizenship = NORWEGIAN -> person[i].house_position = 0
+    !isundefined(houses[i].citizenship)
+    & houses[i].citizenship = NORWEGIAN -> i = 0
 
   assume "the man who smnokes Chesterfields lives in the house next to the man with the fox"
-    forall j: id do
-      !isundefined(person[i].brand) & !isundefined(person[j].pet)
-      & !isundefined(person[i].house_position) & !isundefined(person[j].house_position)
-      & person[i].brand = CHESTERFIELDS & person[j].pet = FOX
-      -> person[i].house_position = person[j].house_position + 1
-       | person[j].house_position = person[i].house_position + 1
+    forall j: 0 .. 4 do
+      !isundefined(houses[i].brand) & !isundefined(houses[j].pet)
+      & houses[i].brand = CHESTERFIELDS & houses[j].pet = FOX
+      -> i = j + 1 | j = i + 1
     end
 
   assume "Kools are smoked in the house next to the house where the horse is kept"
-    forall j: id do
-      !isundefined(person[i].brand) & !isundefined(person[j].pet)
-      & !isundefined(person[i].house_position) & !isundefined(person[j].house_position)
-      & person[i].brand = KOOLS & person[j].pet = HORSE
-      -> person[i].house_position = person[j].house_position + 1
-       | person[j].house_position = person[i].house_position + 1
+    forall j: 0 .. 4 do
+      !isundefined(houses[i].brand) & !isundefined(houses[j].pet)
+      & houses[i].brand = KOOLS & houses[j].pet = HORSE
+      -> i = j + 1 | j = i + 1
     end
 
   assume "the Lucky Strike smoker drinks orange juice"
-    !isundefined(person[i].brand) & !isundefined(person[i].beverage)
-    & person[i].brand = LUCKYSTRIKE -> person[i].beverage = ORANGEJUICE
+    !isundefined(houses[i].brand) & !isundefined(houses[i].beverage)
+    & houses[i].brand = LUCKYSTRIKE -> houses[i].beverage = ORANGEJUICE
 
   assume "the Japanese smokes Parliaments"
-    !isundefined(person[i].citizenship) & !isundefined(person[i].brand)
-    & person[i].citizenship = JAPANESE -> person[i].brand = PARLIAMENTS
+    !isundefined(houses[i].citizenship) & !isundefined(houses[i].brand)
+    & houses[i].citizenship = JAPANESE -> houses[i].brand = PARLIAMENTS
 
   assume "the Norwegian lives next to the blue house"
-    forall j: id do
-      !isundefined(person[i].citizenship) & !isundefined(person[j].paint)
-      & !isundefined(person[i].house_position) & !isundefined(person[j].house_position)
-      & person[i].citizenship = NORWEGIAN & person[j].paint = BLUE
-      -> person[i].house_position = person[j].house_position + 1
-       | person[j].house_position = person[i].house_position  + 1
+    forall j: 0 .. 4 do
+      !isundefined(houses[i].citizenship) & !isundefined(houses[j].paint)
+      & houses[i].citizenship = NORWEGIAN & houses[j].paint = BLUE
+      -> i = j + 1 | j = i + 1
     end
 
 end
 
 -- terminate as soon as everything in the model has been chosen
 invariant
-  exists i: id do isundefined(person[i].citizenship)
-                | isundefined(person[i].house_position)
-                | isundefined(person[i].paint)
-                | isundefined(person[i].pet)
-                | isundefined(person[i].brand)
-                | isundefined(person[i].beverage)
-               end
+  exists i: 0 .. 4 do isundefined(houses[i].citizenship)
+                    | isundefined(houses[i].paint)
+                    | isundefined(houses[i].pet)
+                    | isundefined(houses[i].brand)
+                    | isundefined(houses[i].beverage)
+                   end
