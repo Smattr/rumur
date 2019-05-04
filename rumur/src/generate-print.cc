Smattr/rumur

commit d783655eae837b805b69185d1d198ea142825973

ensure handle offsets are only 0-7
We effectively canonicalise handles by ensuring the base always points at the
first byte we would read and the offset is then only the bit offset within this
byte. The purpose of this is to eventually allow reducing the offset member to 3
bits.

Github: related to #44 "drop width member of handle"

rumur/src/generate-print.cc
@@ -219,9 +219,10 @@ class Generator : public ConstTypeTraversal {
 
  private:
   std::string derive_handle(mpz_class offset, mpz_class width) const {
-    return "((struct handle){ .base = " + handle + ".base, .offset = " + handle
-      + ".offset + ((size_t)" + offset.get_str() + "), .width = "
-      + width.get_str() + "ull })";
+    return "((struct handle){ .base = " + handle + ".base + (" + handle
+      + ".offset + ((size_t)" + offset.get_str() + ")) / CHAR_BIT, .offset = ("
+      + handle + ".offset + ((size_t)" + offset.get_str() + ")) % CHAR_BIT, "
+      + ".width = " + width.get_str() + "ull })";
   }
 
   std::string to_previous() const {

rumur/resources/header.c
@@ -811,8 +811,8 @@ static __attribute__((unused)) struct handle state_handle(const struct state *s,
     "bounds handle in state_handle()");
 
   return (struct handle){
-    .base = (uint8_t*)s->data,
-    .offset = offset,
+    .base = (uint8_t*)s->data + offset / CHAR_BIT,
+    .offset = offset % CHAR_BIT,
     .width = width,
   };
 }
@@ -1260,8 +1260,8 @@ static __attribute__((unused)) struct handle handle_narrow(struct handle h,
   assert(!ADD(h.offset, offset, &r) && "narrowing handle overflows a size_t");
 
   return (struct handle){
-    .base = h.base,
-    .offset = h.offset + offset,
+    .base = h.base + (h.offset + offset) / CHAR_BIT,
+    .offset = (h.offset + offset) % CHAR_BIT,
     .width = width,
   };
 }
@@ -1289,8 +1289,8 @@ static __attribute__((unused)) struct handle handle_index(const char *context,
   assert(!ADD(root.offset, r2, &r) && "indexing handle overflows a size_t");
 
   return (struct handle){
-    .base = root.base,
-    .offset = root.offset + r2,
+    .base = root.base + (root.offset + r2) / CHAR_BIT,
+    .offset = (root.offset + r2) % CHAR_BIT,
     .width = element_width,
   };
 }

rumur/src/generate-stmt.cc
@@ -17,8 +17,9 @@ static void clear(std::ostream &out, const rumur::TypeExpr &t,
   const std::string indent = std::string(2 * (depth + 1), ' ');
 
   if (t.is_simple()) {
-    out << indent << "handle_write_raw((struct handle){ .base = root.base, "
-      << ".offset = root.offset + " << offset << ", .width = "
+    out << indent << "handle_write_raw((struct handle){ .base = root.base + "
+      << "(root.offset + " << offset << ") / CHAR_BIT, .offset = "
+      << "(root.offset + " << offset << ") % CHAR_BIT, .width = "
       << t.width() << "ull }, 1);\n";
 
     return;

commit 94ef1dec8a82d643dba459d97af3870c9e325528

fix: correct previous value read when determining deltas to print
When printing a diff counter-example trace (the default), some values that did
not change were being re-printed. That is, sometimes there would be no delta in
the value of a state component between one state and the next but its value
would be printed anyway. The underlying problem was that the handle used to read
from the previous state was incorrect. More specifically, this handle was always
using a base value of the start of the state data rather than the byte in which
the current field started.

This bug was introduced in d783655eae837b805b69185d1d198ea142825973 when state
handles were changed to use a closer base. We missed that this left the
calculation of the handle to the previous state in state_print incorrect.

Github: closes #125 "diff traces over share with arrays"

rumur/src/generate-print.cc
@@ -226,7 +226,8 @@ class Generator : public ConstTypeTraversal {
   }
 
   std::string to_previous() const {
-    return "((struct handle){ .base = (uint8_t*)previous->data, .offset = "
+    return "((struct handle){ .base = (uint8_t*)previous->data + (" + handle
+      + ".base - (const uint8_t*)s->data), .offset = "
       + handle + ".offset, .width = " + handle + ".width })";
   }
 };