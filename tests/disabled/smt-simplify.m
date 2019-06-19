5a33d50811b891c0cbca7d0db875d80c2d7d6b90
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-06-18 20:10:33 -0700

    add a model for testing SMT simplification
    Github: related to #130 "interact with SMT solver"

---
+++ tests/disabled/smt-simplify.m
@@ -0,0 +1,19 @@
+/* This model tests whether Rumur is capable of simplifying simple conditions at
+ * code generation time. If it is, then it will replace the `y = y` check with
+ * true and this model will pass. If not, the check will remain and cause a read
+ * of an undefined value at runtime.
+ */
+
+var
+  x: boolean;
+  y: boolean;
+
+startstate begin
+  x := true;
+end;
+
+rule begin
+  if y = y then
+    x := !x;
+  end;
+end;
