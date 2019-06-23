45f56b3d06759bd9a0e6343334b5fa2bf2161f2a
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-06-22 17:58:00 -0700

    add support for running an external command
    Github: related to #130 "interact with SMT solver"

---
+++ rumur/src/process.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+/* Run an external process, pass it the given input on stdin and wait for it to
+ * finish. Its stdout data is reported via the output parameter. Note that there
+ * is currently no reporting of the exit status of the process.
+ */
+int run(const std::vector<std::string> &args, const std::string &input,
+  std::string &output);
