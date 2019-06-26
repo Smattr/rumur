dcb3559fbe03014bdf353649f390fc368b7e813c
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-06-25 19:54:40 -0700

    add reusable boolean constants
    This will save us having to reconstruct these repeatedly when we just want to
    ask the question "is the expression I have the literal true or false?"
    
    Github: related to #130 "interact with SMT solver"
    Github: related to #139 "avoid calling SMT solver for "true" and "false""

--- librumur/src/Boolean.cc
+++ librumur/src/Boolean.cc
@@ -2,7 +2,9 @@
 #include "location.hh"
 #include <memory>
 #include <rumur/Boolean.h>
+#include <rumur/Decl.h>
 #include <rumur/Expr.h>
+#include <rumur/Number.h>
 #include <rumur/Ptr.h>
 #include <rumur/TypeExpr.h>
 #include <string>
@@ -14,5 +16,17 @@ namespace rumur {
 const Ptr<Enum> Boolean = Ptr<Enum>::make(
   std::vector<std::pair<std::string, location>>(
     { {"false", location()}, {"true", location()} }), location());
+
+const Ptr<Expr> False = Ptr<ExprID>::make("false",
+  Ptr<ConstDecl>::make("boolean",
+    Ptr<Number>::make(0, location()),
+  Boolean, location()),
+location());
+
+const Ptr<Expr> True = Ptr<ExprID>::make("true",
+  Ptr<ConstDecl>::make("boolean",
+    Ptr<Number>::make(1, location()),
+  Boolean, location()),
+location());
 
 }
