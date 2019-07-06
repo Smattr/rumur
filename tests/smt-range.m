ecba94100643b011759a103e97337646d1155eb7
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-07-06 11:34:37 -0700

    add a test of the SMT bridge on ranges

---
+++ tests/smt-range.m
@@ -0,0 +1,20 @@
+-- rumur_flags: ['--smt-simplification', 'on'] + smt_args()
+-- skip_reason: 'no SMT solver available' if len(smt_args()) == 0 else None
+
+-- test that the SMT bridge can deal with integer range types
+
+var
+  x: 2 .. 10;
+  y: boolean;
+
+startstate begin
+  y := true;
+end;
+
+rule begin
+  -- if the SMT bridge can deal with integer ranges, it should turn the
+  -- following condition into `true`, avoiding a read of x which is undefined
+  if x > 1 then
+    y := !y;
+  end;
+end;
