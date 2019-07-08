d4ae6d2c88cf0ca5a4e2a4f1f94b375d1405b2a5
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-07-07 18:08:29 -0700

    add a facility for counting the number of assume statements
    Github: related to #141 "segfault with misc/pending-queue-4k.m"

---
+++ rumur/src/assume-statements-count.cc
@@ -0,0 +1,29 @@
+#include <cstddef>
+#include "assume-statements-count.h"
+#include <rumur/rumur.h>
+
+using namespace rumur;
+
+unsigned long assume_statements_count(const Model &model) {
+
+  // define a traversal for counting assume statements
+  class AssumeCounter : public ConstTraversal {
+
+   public:
+    unsigned long count = 0;
+
+    void visit_propertystmt(const PropertyStmt &n) final {
+      if (n.property.category == Property::ASSUMPTION)
+        count++;
+      // no need to descend into children
+    }
+
+    virtual ~AssumeCounter() = default;
+  };
+
+  // use the counter to find how many assume statments we have
+  AssumeCounter ac;
+  ac.dispatch(model);
+
+  return ac.count;
+}
