Smattr/rumur

commit 79579fd5ee7cc3c120439b5d3187a09ffd5dcd6e

use raw_value_t when reading and writing raw values
Github: related to #23 "Use native scalars of variable size"

rumur/src/generate-quantifier.cc
@@ -36,9 +36,9 @@ void generate_quantifier_header(std::ostream &out, const Quantifier &q) {
    */
   out
     << "{\n"
-    << "  const value_t step = ";
+    << "  const raw_value_t step = (raw_value_t)";
   if (q.step == nullptr) {
-    out << "VALUE_C(1)";
+    out << "1";
   } else {
     generate_rvalue(out, *q.step);
   }
@@ -46,7 +46,7 @@ void generate_quantifier_header(std::ostream &out, const Quantifier &q) {
 
   // Similar for the upper and lower bounds.
 
-  out << "  const value_t lb = ";
+  out << "  const raw_value_t lb = (raw_value_t)";
   if (q.type == nullptr) {
     assert(q.from != nullptr);
     generate_rvalue(out, *q.from);
@@ -55,7 +55,7 @@ void generate_quantifier_header(std::ostream &out, const Quantifier &q) {
   }
   out << ";\n";
 
-  out << "  const value_t ub = ";
+  out << "  const raw_value_t ub = (raw_value_t)";
   if (q.type == nullptr) {
     assert(q.to != nullptr);
     generate_rvalue(out, *q.to);
@@ -64,12 +64,12 @@ void generate_quantifier_header(std::ostream &out, const Quantifier &q) {
   }
   out << ";\n";
 
-  out << "  for (value_t " << counter << " = lb; " << counter << " <= ub; "
+  out << "  for (raw_value_t " << counter << " = 1; " << counter << " <= ub - lb + 1; "
     << counter << " += step) {\n"
     << "    uint8_t " << block << "[BITS_TO_BYTES(" << width << ")] = { 0 };\n"
     << "    struct handle " << handle << " = { .base = " << block
       << ", .offset = 0, .width = " << width << " };\n"
-    << "    handle_write_raw(" << handle << ", " << counter << " - lb + 1);\n";
+    << "    handle_write_raw(" << handle << ", " << counter << ");\n";
 }
 
 void generate_quantifier_footer(std::ostream &out, const Quantifier &q) {
@@ -77,8 +77,8 @@ void generate_quantifier_footer(std::ostream &out, const Quantifier &q) {
   const std::string counter = "_ru1_" + q.name;
 
   out
-    << "    if (VALUE_MAX - step < ub && " << counter
-      << " > VALUE_MAX - step) {\n"
+    << "    if (RAW_VALUE_MAX - step < ub - lb + 1 && " << counter
+      << " > RAW_VALUE_MAX - step) {\n"
     << "      break;\n"
     << "    }\n"
     << "  }\n"

rumur/resources/header.c
@@ -15,6 +15,7 @@
 #endif
 
 #define value_to_string(v) ((value_t)(v))
+#define raw_value_to_string(v) ((raw_value_t)(v))
 
 /* A more powerful assert that treats the assertion as an assumption when
  * assertions are disabled.
@@ -822,13 +823,13 @@ static __attribute__((unused)) struct handle state_handle(const struct state *s,
 // TODO: The logic in this function is complex and fiddly. It would be desirable
 // to have a proof in, e.g. Z3, that the manipulations it's doing actually yield
 // the correct result.
-static value_t handle_read_raw(struct handle h) {
+static raw_value_t handle_read_raw(struct handle h) {
 
   // FIXME: When we get a user-configurable value type, users will be able to
   // cause this assertion to fail, so we should validate the necessary
   // conditions at code generation time.
-  assert(h.width <= sizeof(value_t) * 8 && "read of a handle to a value that "
-    "is larger than our value type");
+  assert(h.width <= sizeof(raw_value_t) * 8 && "read of a handle to a value "
+    "that is larger than our raw value type");
 
   ASSERT(h.width <= MAX_SIMPLE_WIDTH && "read of a handle that is larger than "
     "the maximum width of a simple type in this model");
@@ -908,10 +909,10 @@ static value_t handle_read_raw(struct handle h) {
       low &= mask;
     }
 
-    value_t v = (value_t)low;
+    raw_value_t v = (raw_value_t)low;
 
-    TRACE(TC_HANDLE_READS, "read value %" PRIVAL " from handle { %p, %zu, %zu }",
-      value_to_string(v), h.base, h.offset, h.width);
+    TRACE(TC_HANDLE_READS, "read value %" PRIRAWVAL " from handle { %p, %zu, %zu }",
+      raw_value_to_string(v), h.base, h.offset, h.width);
 
     return v;
   }
@@ -955,19 +956,19 @@ static value_t handle_read_raw(struct handle h) {
     low &= mask;
   }
 
-  value_t v = (value_t)low;
+  raw_value_t v = (raw_value_t)low;
 
-  TRACE(TC_HANDLE_READS, "read value %" PRIVAL " from handle { %p, %zu, %zu }",
-    value_to_string(v), h.base, h.offset, h.width);
+  TRACE(TC_HANDLE_READS, "read value %" PRIRAWVAL " from handle { %p, %zu, %zu }",
+    raw_value_to_string(v), h.base, h.offset, h.width);
 
   return v;
 }
 
-static value_t decode_value(value_t lb, value_t ub, value_t v) {
+static value_t decode_value(value_t lb, value_t ub, raw_value_t v) {
 
-  value_t dest = v;
+  value_t dest = 0;
 
-  bool r __attribute__((unused)) = SUB(dest, 1, &dest) || ADD(dest, lb, &dest)
+  bool r __attribute__((unused)) = SUB(v, 1, &v) || ADD(v, lb, &dest)
     || dest < lb || dest > ub;
 
   ASSERT(!r && "read of out-of-range value");
@@ -989,7 +990,7 @@ static __attribute__((unused)) value_t handle_read(const char *context,
     || sizeof(s->data) * CHAR_BIT - h.width >= h.offset) /* in bounds */
     && "out of bounds read in handle_read()");
 
-  value_t dest = handle_read_raw(h);
+  raw_value_t dest = handle_read_raw(h);
 
   if (dest == 0) {
     error(s, "%sread of undefined value in %s%s%s", context, name,
@@ -999,16 +1000,16 @@ static __attribute__((unused)) value_t handle_read(const char *context,
   return decode_value(lb, ub, dest);
 }
 
-static void handle_write_raw(struct handle h, value_t value) {
+static void handle_write_raw(struct handle h, raw_value_t value) {
 
-  assert(h.width <= sizeof(value_t) * 8 && "write of a handle to a value that "
-    "is larger than our value type");
+  assert(h.width <= sizeof(raw_value_t) * 8 && "write of a handle to a value "
+    "that is larger than our raw value type");
 
   ASSERT(h.width <= MAX_SIMPLE_WIDTH && "write of a handle that is larger than "
     "the maximum width of a simple type in this model");
 
-  TRACE(TC_HANDLE_WRITES, "writing value %" PRIVAL " to handle { %p, %zu, %zu }",
-    value_to_string(value), h.base, h.offset, h.width);
+  TRACE(TC_HANDLE_WRITES, "writing value %" PRIRAWVAL " to handle { %p, %zu, %zu }",
+    raw_value_to_string(value), h.base, h.offset, h.width);
 
   if (h.width == 0) {
     return;
@@ -1160,13 +1161,13 @@ static __attribute__((unused)) void handle_write(const char *context,
     || sizeof(s->data) * CHAR_BIT - h.width >= h.offset) /* in bounds */
     && "out of bounds write in handle_write()");
 
-  if (value < lb || value > ub || SUB(value, lb, &value) ||
-      ADD(value, 1, &value)) {
+  raw_value_t r;
+  if (value < lb || value > ub || SUB(value, lb, &r) || ADD(r, 1, &r)) {
     error(s, "%swrite of out-of-range value into %s%s%s", context, name,
       rule_name == NULL ? "" : " within ", rule_name == NULL ? "" : rule_name);
   }
 
-  handle_write_raw(h, value);
+  handle_write_raw(h, r);
 }
 
 static __attribute__((unused)) void handle_zero(struct handle h) {
@@ -1297,7 +1298,7 @@ static __attribute__((unused)) struct handle handle_index(const char *context,
 }
 
 static __attribute__((unused)) value_t handle_isundefined(struct handle h) {
-  value_t v = handle_read_raw(h);
+  raw_value_t v = handle_read_raw(h);
 
   return v == 0;
 }

rumur/src/generate-expr.cc
@@ -345,14 +345,14 @@ class Generator : public ConstExprTraversal {
 
           *out
             << "{ "
-            << "value_t v = handle_read_raw(";
+            << "raw_value_t v = handle_read_raw(";
           generate_lvalue(*out, *a);
           *out << "); "
             << "if (v != 0 && (v + " << lba << " - 1 < " << lb << " || v + "
               << lba << " - 1 > " << ub << ")) { "
             << "error(s, \"call to function %s passed an out-of-range value "
-              << "%\" PRIVAL \" to parameter " << (index + 1) << "\", \""
-              << n.name << "\", value_to_string(v + " << lba << " - 1)); "
+              << "%\" PRIRAWVAL \" to parameter " << (index + 1) << "\", \""
+              << n.name << "\", raw_value_to_string(v + " << lba << " - 1)); "
             << "} "
             << "handle_write_raw(" << handle << ", v == 0 ? v : (v + " << lba
               << " - " << lb << ")); "

rumur/src/generate-print.cc
@@ -91,8 +91,8 @@ class Generator : public ConstTypeTraversal {
 
     *out
       << "{\n"
-      << "  value_t v = handle_read_raw(" << handle << ");\n"
-      << "  value_t v_previous = VALUE_C(0);\n";
+      << "  raw_value_t v = handle_read_raw(" << handle << ");\n"
+      << "  raw_value_t v_previous = 0;\n";
     if (!support_diff)
       *out << "  const struct state *previous = NULL;\n";
     *out
@@ -136,8 +136,8 @@ class Generator : public ConstTypeTraversal {
 
     *out
       << "{\n"
-      << "  value_t v = handle_read_raw(" << handle << ");\n"
-      << "  value_t v_previous = VALUE_C(0);\n";
+      << "  raw_value_t v = handle_read_raw(" << handle << ");\n"
+      << "  raw_value_t v_previous = 0;\n";
     if (!support_diff)
       *out << "  const struct state *previous = NULL;\n";
     *out
@@ -181,8 +181,8 @@ class Generator : public ConstTypeTraversal {
 
     *out
       << "{\n"
-      << "  value_t v = handle_read_raw(" << handle << ");\n"
-      << "  value_t v_previous = VALUE_C(0);\n";
+      << "  raw_value_t v = handle_read_raw(" << handle << ");\n"
+      << "  raw_value_t v_previous = 0;\n";
     if (!support_diff)
       *out << "  const struct state *previous = NULL;\n";
     *out

rumur/src/symmetry-reduction.cc
@@ -36,10 +36,10 @@ static void generate_apply_swap(std::ostream &out, const std::string &offset_a,
 
     out
       << indent << "if (" << offset_a << " != " << offset_b << ") {\n"
-      << indent << "  value_t a = handle_read_raw(state_handle(s, " << offset_a
-        << ", " << t->width() << "ull));\n"
-      << indent << "  value_t b = handle_read_raw(state_handle(s, " << offset_b
-        << ", " << t->width() << "ull));\n"
+      << indent << "  raw_value_t a = handle_read_raw(state_handle(s, "
+        << offset_a << ", " << t->width() << "ull));\n"
+      << indent << "  raw_value_t b = handle_read_raw(state_handle(s, "
+        << offset_b << ", " << t->width() << "ull));\n"
       << indent << "  handle_write_raw(state_handle(s, " << offset_b
         << ", " << t->width() << "ull), a);\n"
       << indent << "  handle_write_raw(state_handle(s, " << offset_a
@@ -101,11 +101,11 @@ static void generate_swap_chunk(std::ostream &out, const TypeExpr &t,
 
         out
           << indent << "if (x != y) {\n"
-          << indent << "  value_t v = handle_read_raw(" << h << ");\n"
+          << indent << "  raw_value_t v = handle_read_raw(" << h << ");\n"
           << indent << "  if (v != 0) {\n"
-          << indent << "    if (v - 1 == (value_t)x) {\n"
+          << indent << "    if (v - 1 == (raw_value_t)x) {\n"
           << indent << "      handle_write_raw(" << h << ", y + 1);\n"
-          << indent << "    } else if (v - 1 == (value_t)y) {\n"
+          << indent << "    } else if (v - 1 == (raw_value_t)y) {\n"
           << indent << "      handle_write_raw(" << h << ", x + 1);\n"
           << indent << "    }\n"
           << indent << "  }\n"
@@ -320,9 +320,9 @@ static void generate_apply_compare(std::ostream &out, const TypeExpr &type,
 
       out
         << indent << "if (" << offset_a << " != " << offset_b << ") {\n"
-        << indent << "  value_t a = handle_read_raw(state_handle(s, " << offset_a
+        << indent << "  raw_value_t a = handle_read_raw(state_handle(s, " << offset_a
           << ", " << t->width() << "ull));\n"
-        << indent << "  value_t b = handle_read_raw(state_handle(s, " << offset_b
+        << indent << "  raw_value_t b = handle_read_raw(state_handle(s, " << offset_b
           << ", " << t->width() << "ull));\n"
         << indent << "  if (a < b) {\n"
         << indent << "    return -1;\n"
@@ -402,13 +402,13 @@ static void generate_compare_chunk(std::ostream &out, const TypeExpr &t,
          */
         << indent << "{\n"
 
-        << indent << "  value_t v = handle_read_raw(state_handle(s, " << offset
+        << indent << "  raw_value_t v = handle_read_raw(state_handle(s, " << offset
           << ", " << width << "));\n"
 
         << indent << "  if (v != 0) { /* ignored 'undefined' */\n"
-        << indent << "    if (v - 1 == (value_t)x) {\n"
+        << indent << "    if (v - 1 == (raw_value_t)x) {\n"
         << indent << "      return -1;\n"
-        << indent << "    } else if (v - 1 == (value_t)y) {\n"
+        << indent << "    } else if (v - 1 == (raw_value_t)y) {\n"
         << indent << "      return 1;\n"
         << indent << "    }\n"
         << indent << "  }\n"

commit 2329056db14d87301bba9c56115cdd4539bed1af

fix: perform quantifier loop comparison as signed
This was leading to the misc/pending-queue.m model terminating early with a
surprisingly small number of states. Some more explanation from the Github
issue:

  The misc/pending-queue.m model produces many fewer states (81) than it used to
  (147487952), which is unexpected. Bisecting indicates 79579fd is the first bad
  commit.
  ...
  The problem here is that commit 79579fd inadvertently caused the comparison in
  the loop emitted for a quantifier to become unsigned. A quantifier emits code
  that previously looked something like:

    const value_t step = VALUE_C(1);
    const value_t lb = VALUE_C(-1);
    const value_t ub = VALUE_C(3);
    for (value_t _ru1_i = lb; _ru_i <= ub; _ru_i += step) {
    ...

  After that commit, the emitted code looks like:

    const raw_value_t step = (raw_value_t)1;
    const raw_value_t lb = (raw_value_t)VALUE_C(-1);
    const raw_value_t ub = (raw_value_t)VALUE_C(3);
    for (raw_value_t _ru1_i = 1; _ru_i <= ub - lb + 1; _ru_i += step) {
    ...

  Now everything generally works out fine except in cases like the above when
  the range spans positive and negative values. Notice that the first iteration
  of the loop in the second case already has a false condition. The problem here
  is that the comparison was previously comparing value_ts and now it's
  comparing raw_value_ts. In a typical model this goes from comparing int8_t
  values to uint8_t values.

Github: closes #142 "pending-queue.m terminates exploration too early"

rumur/src/generate-quantifier.cc
@@ -64,8 +64,9 @@ void generate_quantifier_header(std::ostream &out, const Quantifier &q) {
   }
   out << ";\n";
 
-  out << "  for (raw_value_t " << counter << " = 1; " << counter << " <= ub - lb + 1; "
-    << counter << " += step) {\n"
+  out
+    << "  for (raw_value_t " << counter << " = 1; (value_t)" << counter
+      << " <= (value_t)(ub - lb + 1); " << counter << " += step) {\n"
     << "    uint8_t " << block << "[BITS_TO_BYTES(" << width << ")] = { 0 };\n"
     << "    struct handle " << handle << " = { .base = " << block
       << ", .offset = 0, .width = " << width << " };\n"