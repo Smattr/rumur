Smattr/rumur

commit d2949e3516c613f6183ce3219d403e4b3e96add9

fix -Wsign-compare warning on function parameter check

rumur/src/generate-expr.cc
@@ -348,14 +348,15 @@ class Generator : public ConstExprTraversal {
             << "raw_value_t v = handle_read_raw(";
           generate_lvalue(*out, *a);
           *out << "); "
-            << "if (v != 0 && (v + " << lba << " - 1 < " << lb << " || v + "
-              << lba << " - 1 > " << ub << ")) { "
+            << "value_t v2;\n"
+            << "if (v != 0 && (ADD(v, " << lba << ", &v2) || SUB(v2, 1, &v2) "
+              << "|| v2 < " << lb << " || v2 > " << ub << ")) { "
             << "error(s, \"call to function %s passed an out-of-range value "
               << "%\" PRIRAWVAL \" to parameter " << (index + 1) << "\", \""
               << n.name << "\", raw_value_to_string(v + " << lba << " - 1)); "
             << "} "
-            << "handle_write_raw(" << handle << ", v == 0 ? v : (v + " << lba
-              << " - " << lb << ")); "
+            << "handle_write_raw(" << handle << ", v == 0 ? v : (v2 + " << lba
+              << " + 1 - " << lb << ")); "
             << "} ";
 
         } else if (method == 3) {

commit 1a7342956115a691118b315bf8ea1cb551f718f9

fix: incorrect value calculation for scalars passed to functions
d2949e3516c613f6183ce3219d403e4b3e96add9 introduced a bug wherein the raw value
written to a handle then passed to a function call was computed incorrectly. The
problem was essentially that additions and subtractions were swapped for calls
to ADD and SUB without noting that the latter actually write to their
destination.

Interestingly this was not caught directly by anything in the test suite but was
detected by GCC issuing -Wsign-compare warnings for the model
out-of-range-function-parameter2.m.

rumur/src/generate-expr.cc
@@ -348,15 +348,16 @@ class Generator : public ConstExprTraversal {
             << "raw_value_t v = handle_read_raw(";
           generate_lvalue(*out, *a);
           *out << "); "
-            << "value_t v2;\n"
-            << "if (v != 0 && (ADD(v, " << lba << ", &v2) || SUB(v2, 1, &v2) "
-              << "|| v2 < " << lb << " || v2 > " << ub << ")) { "
+            << "raw_value_t v2;\n"
+            << "value_t v3;\n"
+            << "if (v != 0 && (SUB(v, 1, &v2) || ADD(v2, " << lba << ", &v3) "
+              << "|| v3 < " << lb << " || v3 > " << ub << ")) { "
             << "error(s, \"call to function %s passed an out-of-range value "
               << "%\" PRIRAWVAL \" to parameter " << (index + 1) << "\", \""
               << n.name << "\", raw_value_to_string(v + " << lba << " - 1)); "
             << "} "
-            << "handle_write_raw(" << handle << ", v == 0 ? v : (v2 + " << lba
-              << " + 1 - " << lb << ")); "
+            << "handle_write_raw(" << handle << ", v == 0 ? v : "
+              << "((raw_value_t)(v3 - " << lb << ") + 1)); "
             << "} ";
 
         } else if (method == 3) {