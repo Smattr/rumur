2d37cb409dc15a35a8626a1ce9d2a8b98b341fba
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-08-01 18:09:03 -0700

    add some test cases in preparation for array support in the SMT bridge

---
+++ tests/disabled/smt-array-enum-value2.m
@@ -0,0 +1,26 @@
+-- rumur_flags: ['--smt-simplification', 'on'] + smt_args()
+-- skip_reason: 'no SMT solver available' if len(smt_args()) == 0 else None
+
+-- variant of smt-array-enum-value.m that pre-defines the enum
+
+type
+  e: enum { A, B };
+  t: array[0 .. 1] of e;
+
+var
+  x: t;
+  y: boolean;
+
+startstate begin
+  y := true;
+end;
+
+rule begin
+  /* if the SMT bridge is working correctly, it will simplify the condition in
+   * the following expression to `true` avoiding the read of an undefined
+   * variable
+   */
+  if x[0] = x[0] then
+    y := !y;
+  end;
+end;
---
+++ tests/disabled/smt-array-range2.m
@@ -0,0 +1,26 @@
+-- rumur_flags: ['--smt-simplification', 'on'] + smt_args()
+-- skip_reason: 'no SMT solver available' if len(smt_args()) == 0 else None
+
+-- slight variant on smt-array-range.m with a pre-defined type
+
+type
+  r: 0 .. 1;
+  t: array[r] of r;
+
+var
+  x: t;
+  y: boolean;
+
+startstate begin
+  y := true;
+end;
+
+rule begin
+  /* if the SMT bridge is working correctly, it will simplify the condition in
+   * the following expression to `true` avoiding the read of an undefined
+   * variable
+   */
+  if x[0] = x[0] then
+    y := !y;
+  end;
+end;
