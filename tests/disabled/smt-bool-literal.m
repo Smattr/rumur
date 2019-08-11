1862337356ea88f076214c2436b7e3c2b4283a05
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-08-10 19:17:42 -0700

    add a test case for a bool literal SMT bug

---
+++ tests/disabled/smt-bool-literal.m
@@ -0,0 +1,27 @@
+-- rumur_flags: ['--smt-simplification', 'on'] + smt_args()
+-- skip_reason: 'no SMT solver available' if len(smt_args()) == 0 else None
+
+/* This model attempts to provoke a bug first observed on commit
+ * 5d4f1939ddc5d5d9336f0ce35e953c51e8b5aeca. The SMT bridge did not correctly
+ * understand the boolean literals and would form malformed problems when seeing
+ * them. If this problem has been introduced, this model will fail verification
+ * by reading an undefined variable.
+ */
+
+var
+  x: boolean;
+  y: boolean;
+
+startstate begin
+  x := true;
+end;
+
+rule begin
+  /* if the SMT bridge correctly deals with boolean literals, the following
+   * expression should be simplified to true and avoid the access to an
+   * undefined variable
+   */
+  if y = true | y = false then
+    x := !x;
+  end;
+end;
