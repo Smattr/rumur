f05c17f4fb1ebe1c316846acd28ddbcb99b3d99e
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-08-16 20:22:03 -0700

    fix Linux compilation

---
+++ rumur/src/environ.cc
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include "environ.h"
+
+#ifdef __APPLE__
+    #include <crt_externs.h>
+#endif
+
+char **get_environ(void) {
+#ifdef __APPLE__
+  // on macOS, environ is not directly accessible
+  return *_NSGetEnviron();
+#else
+  /* some platforms fail to expose environ in a header (e.g. FreeBSD), so
+   * declare it ourselves and assume it will be available when linking
+   */
+  extern char **environ;
+
+  return environ;
+#endif
+}
--- rumur/src/environ.h
+++ rumur/src/environ.h
@@ -1,19 +1,4 @@
 #pragma once
 
-#ifdef __APPLE__
-    #include <crt_externs.h>
-#endif
-
-static inline char **get_environ() {
-#if __APPLE__
-  // on macOS, environ is not directly accessible
-  return *_NSGetEnviron();
-#else
-  /* some platforms fail to expose environ in a header (e.g. FreeBSD), so
-   * declare it ourselves and assume it will be available when linking
-   */
-  extern char **environ;
-
-  return environ;
-#endif
-}
+// get a pointer to the environ variable
+char **get_environ(void);
