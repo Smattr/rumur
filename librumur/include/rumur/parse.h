d632539c6229a768cb475aec47666f927dfd30e3
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-11-08 21:52:42 -0800

    fix outdated comment
    This function no longer does "one shot" parsing.

--- librumur/include/rumur/parse.h
+++ librumur/include/rumur/parse.h
@@ -7,14 +7,7 @@
 
 namespace rumur {
 
-/* Parse in a model from an input stream. Throws Errors on parsing errors.
- *
- * This is a one-shot parsing API. That is, it fully parses the input model and
- * validates it. If you want to do something more flexible like parse a partial
- * model or tolerate certain types of errors, you may wish to open code your
- * own parsing logic. I recommend you copy the implementation of this function
- * and tweak as desired.
- */
+// Parse in a model from an input stream. Throws Errors on parsing errors.
 Ptr<Model> parse(std::istream *input);
 
 }
