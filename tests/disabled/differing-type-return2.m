bbcc12bbda94909c5bb6d1be082c19d640f2ef12
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-11-08 21:51:05 -0800

    add note about ambiguous test case

--- tests/disabled/differing-type-return2.m
+++ tests/disabled/differing-type-return2.m
@@ -3,6 +3,11 @@
 -- A variant of differing-type-return.m that should be accepted for code
 -- generation but then should fail at runtime.
 
+-- FIXME: *Should* this actually fail at runtime? The generated code currently
+-- results in a function that returns an rvalue (value_t), so everything works
+-- out fine. If the user had actually written this, it would be clear what they
+-- meant. I'm inclined to think maybe this should just be accepted...?
+
 type
   t1: 0 .. 10;
   t2: 0 .. 11;
