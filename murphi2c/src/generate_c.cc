106b7e831c759cb429ac046ae8ae7de9ba9a9e5e
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-11-21 17:46:53 -0800

    murphi2c: implement model translation

--- murphi2c/src/generate_c.cc
+++ murphi2c/src/generate_c.cc
@@ -6,6 +6,7 @@
 #include <rumur/rumur.h>
 #include <string>
 #include <utility>
+#include <vector>
 
 using namespace rumur;
 
@@ -262,6 +263,34 @@ class CGenerator : public ConstTraversal {
 
   void visit_mod(const Mod &n) final {
     *this << "(" << *n.lhs << " % " << *n.rhs << ")";
+  }
+
+  void visit_model(const Model &n) final {
+
+    // constants, types and variables
+    for (const Ptr<Decl> &d : n.decls) {
+      *this << *d;
+    }
+
+    *this << "\n";
+
+    // functions and procedures
+    for (const Ptr<Function> &f : n.functions) {
+      *this << *f << "\n";
+    }
+
+    // flatten the rules so we do not have to deal with the hierarchy of
+    // rulesets, aliasrules, etc.
+    std::vector<Ptr<Rule>> flattened;
+    for (const Ptr<Rule> &r : n.rules) {
+      std::vector<Ptr<Rule>> rs = r->flatten();
+      flattened.insert(flattened.end(), rs.begin(), rs.end());
+    }
+
+    // startstates, rules, invariants
+    for (const Ptr<Rule> &r : flattened) {
+      *this << *r << "\n";
+    }
   }
 
   void visit_mul(const Mul &n) final {
