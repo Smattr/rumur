a1b6d61f8bab250f175761b1c1ab8e13954efb21
Matthew Fernandez <matthew.fernandez@gmail.com>
2019-12-22 20:50:27 -0800

    release v2019.12.22

--- librumur/src/make-version.py
+++ librumur/src/make-version.py
@@ -12,7 +12,7 @@ from typing import Optional
 
 # The version of the last tagged release of Rumur. This will be used as the
 # version number if no Git information is available.
-LAST_RELEASE = 'v2019.11.24'
+LAST_RELEASE = 'v2019.12.22'
 
 def has_git() -> bool:
   '''
