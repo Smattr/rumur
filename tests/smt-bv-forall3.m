ab111f8e51481ba0df4c9283c38a13cc5270bc47
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-01-18 11:12:22 -0800

    add the equivalent bitvector tests of SMT forall behaviour

---
+++ tests/smt-bv-forall3.m
@@ -0,0 +1,20 @@
+-- rumur_flags: SMT_BV_ARGS
+-- skip_reason: 'no SMT solver available' if len(SMT_BV_ARGS) == 0 else None
+
+-- equivalent of smt-forall3.m but using --smt-bitvectors on
+
+var
+  x: boolean;
+  y: boolean;
+
+startstate begin
+  x := true;
+end;
+
+rule begin
+  -- if the SMT bridge is working correctly, it should simplify the condition as
+  -- a tautology into true, avoiding the read of an undefined variable
+  if forall z := 1 to 2 do z = 1 | z = 2 end | y then
+    x := !x;
+  end;
+end;
