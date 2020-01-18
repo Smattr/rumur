37cfa28ad640757eb42d4e394974ad2630987089
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-01-18 12:27:34 -0800

    fix: don't use pointer compression on Linux x32
    Debian auto builder testing of v2020.01.11-1 exposed that we incorrectly attempt
    pointer compression when using the x32 ABI, under which pointers are 32-bit. We
    now only compress when on Linux x86-64 targeting the standard ABI.

--- rumur/resources/header.c
+++ rumur/resources/header.c
@@ -45,7 +45,7 @@ enum { STATE_SIZE_BYTES = BITS_TO_BYTES(STATE_SIZE_BITS) };
 /* the size of auxliary members of the state struct */
 enum { BOUND_BITS = BITS_FOR(BOUND) };
 #if COUNTEREXAMPLE_TRACE != CEX_OFF || LIVENESS_COUNT > 0
-  #if defined(__linux__) && defined(__x86_64__)
+  #if defined(__linux__) && defined(__x86_64__) && !defined(__ILP32__)
     /* assume 5-level paging, and hence the top 2 bytes of any user pointer are
      * always 0 and not required.
      * https://www.kernel.org/doc/Documentation/x86/x86_64/mm.txt
@@ -977,7 +977,7 @@ static __attribute__((pure)) const struct state *state_previous_get(
 static void state_previous_set(struct state *NONNULL s,
     const struct state *previous) {
 #if PACK_STATE
-#if defined(__linux__) && defined(__x86_64__)
+#if defined(__linux__) && defined(__x86_64__) && !defined(__ILP32__)
   ASSERT(((uintptr_t)previous >> PREVIOUS_BITS) == 0
     && "upper 2 bytes of pointer are non-zero (not using 5-level paging?)");
 #endif
