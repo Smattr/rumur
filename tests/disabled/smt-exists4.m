44a272d9909464739b62a6248a3aab520c484811
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-01-26 14:21:00 -0800

    add some tests of exists in the SMT bridge

---
+++ tests/disabled/smt-exists4.m
@@ -0,0 +1,20 @@
+-- rumur_flags: SMT_ARGS
+-- skip_reason: 'no SMT solver available' if len(SMT_ARGS) == 0 else None
+
+-- a variant of smt-exists.m using an inline range with stepping
+
+var
+  x: boolean;
+  y: boolean;
+
+startstate begin
+  x := true;
+end;
+
+rule begin
+  -- if the SMT bridge is working correctly, it should simplify the condition as
+  -- a tautology into true, avoiding the read of an undefined variable
+  if y | !exists z := 2 to 4 by 2 do z = 3 end then
+    x := !x;
+  end;
+end;
