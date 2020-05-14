9a539f356f8638a9418c72ccd2c4a791e440cf6d
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-05-13 18:52:48 -0700

    bitwise OR test cases
    Github: related to #37 "extended operator support"

---
+++ tests/smt-bv-bor.m
@@ -0,0 +1,20 @@
+-- rumur_flags: self.config['SMT_BV_ARGS']
+-- skip_reason: 'no SMT solver available' if self.config['SMT_BV_ARGS'] is None else None
+
+-- test that the SMT bridge can simplify bitwise OR when using bitvector logic
+
+var
+  x: 1 .. 2
+  y: boolean
+
+startstate begin
+  y := true;
+end
+
+rule begin
+  -- if the SMT bridge can handle bitwise OR, it should simplify the following
+  -- to `true`, avoiding an undefined read
+  if (x | 1) = 1 | (x | 1) = 3 then
+    y := !y;
+  end;
+end
