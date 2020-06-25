168aea1a07134961b4e88c8ce5adbca27ecfee79
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-06-24 18:26:59 -0700

    add scalarset CEX printing test case
    Github: related to #194 "clearer CEX output with scalarsets"

---
+++ tests/scalarset-cex.m
@@ -0,0 +1,30 @@
+-- checker_exit_code: 1
+-- checker_output: None if self.xml or getattr(self, 'multithreaded', False) else re.compile(r'\ba\b.*?\bz:\s*0\b(.|\n)*?\bx\[9]:\s*true\b(.|\n)*?\bc\b.*?\bz:\s*9\b(.|\n)*?\by:\s*t_6', re.MULTILINE)
+
+type
+  t: scalarset(10);
+
+var
+  x: array[t] of boolean;
+  y: t;
+
+startstate begin
+  for z: t do x[z] := false; end;
+end;
+
+ruleset z: t do
+  rule "a" begin
+    x[z] := true;
+  end;
+
+  rule "b" begin
+    x[z] := false;
+  end;
+
+  rule "c" begin
+    y := z;
+  end;
+end;
+
+invariant
+  forall z: t do isundefined(y) | isundefined(x[z]) | y != z | !x[z] end;
