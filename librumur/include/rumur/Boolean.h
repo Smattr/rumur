d41643c43ff5c249d8b2290a314ad060bdafbd00
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-02-04 08:17:05 -0800

    some comments about declarations in Boolean.h

--- librumur/include/rumur/Boolean.h
+++ librumur/include/rumur/Boolean.h
@@ -8,8 +8,13 @@
 
 namespace rumur {
 
+/// the built in boolean type that is implicitly declared in all Murphi models
 extern const Ptr<Enum> Boolean;
 
+/// literals for Murphi “False” and “True”
+///
+/// These are included for convenience, so you can assign to expressions to
+/// these constants if relevant.
 extern const Ptr<Expr> False;
 extern const Ptr<Expr> True;
 
