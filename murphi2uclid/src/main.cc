8686266c656885851761075cf10de89f5805ed8c
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-03-01 21:34:24 -0800

    add basic skeleton of murphi2uclid

---
+++ murphi2uclid/src/main.cc
@@ -0,0 +1,144 @@
+#include "../../common/help.h"
+#include "resources.h"
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <getopt.h>
+#include <iostream>
+#include <memory>
+#include <rumur/rumur.h>
+#include <sstream>
+#include <string>
+#include <sys/stat.h>
+#include <utility>
+
+// a pair of input streams
+using dup_t =
+    std::pair<std::shared_ptr<std::istream>, std::shared_ptr<std::istream>>;
+
+static std::string in_filename = "<stdin>";
+static dup_t in;
+static std::shared_ptr<std::ostream> out;
+
+static void parse_args(int argc, char **argv) {
+
+  for (;;) {
+    static struct option options[] = {
+        // clang-format off
+        { "help",       no_argument,       0, 'h' },
+        { "output",     required_argument, 0, 'o' },
+        { "version",    no_argument,       0, 128 },
+        { 0, 0, 0, 0 },
+        // clange-format on
+    };
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "ho:", options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c) {
+
+    case '?':
+      std::cerr << "run `" << argv[0] << " --help` to see available options\n";
+      exit(EXIT_SUCCESS);
+
+    case 'h': // --help
+      help(doc_murphi2uclid_1, doc_murphi2uclid_1_len);
+      exit(EXIT_SUCCESS);
+
+    case 'o': {
+      auto o = std::make_shared<std::ofstream>(optarg);
+      if (!o->is_open()) {
+        std::cerr << "failed to open " << optarg << "\n";
+        exit(EXIT_FAILURE);
+      }
+      out = o;
+      break;
+    }
+
+    case 128: // --version
+      std::cout << "Murphi2Uclid version " << rumur::get_version() << "\n";
+      exit(EXIT_SUCCESS);
+
+    default:
+      std::cerr << "unexpected error\n";
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (optind == argc - 1) {
+    struct stat buf;
+    if (stat(argv[optind], &buf) < 0) {
+      std::cerr << "failed to open " << argv[optind] << ": " << strerror(errno)
+                << "\n";
+      exit(EXIT_FAILURE);
+    }
+
+    if (S_ISDIR(buf.st_mode)) {
+      std::cerr << "failed to open " << argv[optind]
+                << ": this is a directory\n";
+      exit(EXIT_FAILURE);
+    }
+
+    in_filename = argv[optind];
+
+    auto i = std::make_shared<std::ifstream>(in_filename);
+    auto j = std::make_shared<std::ifstream>(in_filename);
+    if (!i->is_open() || !j->is_open()) {
+      std::cerr << "failed to open " << in_filename << "\n";
+      exit(EXIT_FAILURE);
+    }
+    in = dup_t(i, j);
+  }
+}
+
+static dup_t make_stdin_dup() {
+
+  // read stdin into memory
+  auto buffer = std::make_shared<std::stringstream>();
+  *buffer << std::cin.rdbuf();
+
+  // duplicate the buffer
+  auto copy = std::make_shared<std::istringstream>(buffer->str());
+
+  return dup_t(buffer, copy);
+}
+
+int main(int argc, char **argv) {
+
+  // parse command line options
+  parse_args(argc, argv);
+
+  // if we are reading from stdin, duplicate it so that we can parse it both as
+  // Murphi and for comments
+  if (in.first == nullptr)
+    in = make_stdin_dup();
+
+  // parse input model
+  rumur::Ptr<rumur::Model> m;
+  try {
+    m = rumur::parse(*in.first);
+  } catch (rumur::Error &e) {
+    std::cerr << e.loc << ":" << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  assert(m != nullptr);
+
+  // update unique identifiers within the model
+  m->reindex();
+
+  // check the model is valid
+  try {
+    resolve_symbols(*m);
+    validate(*m);
+  } catch (rumur::Error &e) {
+    std::cerr << e.loc << ":" << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
