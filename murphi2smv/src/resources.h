e52e978515dfed9a99e63e39db5cdc38af804535
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-24 20:00:49 +1100

    port resource generator to C

--- murphi2smv/src/resources.h
+++ murphi2smv/src/resources.h
@@ -2,5 +2,13 @@
 
 #include <cstddef>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const unsigned char doc_murphi2smv_1[];
 extern const size_t doc_murphi2smv_1_len;
+
+#ifdef __cplusplus
+}
+#endif
