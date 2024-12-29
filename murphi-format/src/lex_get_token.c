4cce592269aaa96a115196779029b24f71ba380e
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-29 20:36:11 +1100

    murphi-format: slightly more sane C formatting

--- murphi-format/src/lex_get_token.c
+++ murphi-format/src/lex_get_token.c
@@ -247,7 +247,8 @@ int lex_get_token(lex_t *me, token_t *token) {
   case '{':
     RET(TOKEN_OPEN_BRACE);
 
-    operator: default : if (may_be_operator(0, first)) {
+  default:
+    operator: if (may_be_operator(0, first)) {
       for (size_t i = 1;; ++i) {
         const int c = getc(me->src);
         if (c == EOF) {
