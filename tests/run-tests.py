Smattr/rumur

commit 13e9b51b2b0b3d6338260229cb8a9aa115d2d294

murphi-format: handle smart quotes

murphi-format/src/lex_get_token.c
@@ -134,6 +134,31 @@ int lex_get_token(lex_t *me, token_t *token) {
   case ';':
     RET(TOKEN_SEMI);
 
+  case 0xe2: { // first byte of "“" and "”"
+    const int second = getc(me->src);
+    if (second == EOF) {
+      me->done = true;
+      break;
+    }
+    if (second != 0x80) {
+      ungetc(second, me->src);
+      break;
+    }
+    ACCRUE(second);
+    const int third = getc(me->src);
+    if (third == EOF) {
+      me->done = true;
+      break;
+    }
+    if (third != 0x9c && third != 0x9d) {
+      ungetc(third, me->src);
+      break;
+    }
+    ACCRUE(third);
+    if (third == 0x9d) // we have "”"
+      break;
+  }
+  // fall through
   case '"':
     for (bool escaping = false;;) {
       const int c = getc(me->src);
@@ -142,8 +167,20 @@ int lex_get_token(lex_t *me, token_t *token) {
         break;
       }
       ACCRUE(c);
-      if (c == '"' && !escaping)
-        break;
+      if (fflush(me->stage) < 0)
+        return errno;
+      if (!escaping) {
+        if (me->stage_base[0] == '"') {
+          if (me->stage_base[me->stage_size - 1] == '"')
+            break;
+        }
+        if (me->stage_size >= strlen("“") + strlen("”")) {
+          if (strncmp(me->stage_base, "“", strlen("“")) == 0) {
+            if (strcmp(&me->stage_base[me->stage_size - strlen("”")], "”") == 0)
+              break;
+          }
+        }
+      }
       if (c == '\\') {
         escaping = !escaping;
       } else {

tests/run-tests.py
@@ -1035,6 +1035,18 @@ class MurphiFormat(unittest.TestCase):
 
         self.assertIn(":= ¬y", stdout, "`¬` spaced incorrectly")
 
+    def test_smart_quotes(self):
+        """smart quotes (“”) should be handled correctly"""
+
+        model = "rule begin assert “foo bar” x; end"
+
+        ret, stdout, stderr = run(["murphi-format"], model)
+
+        self.assertEqual(ret, 0, "failed to reflow Murphi snippet")
+        self.assertEqual(stderr, "", "murphi-format printed errors/warnings")
+
+        self.assertIn("assert “foo bar” x", stdout, "smart quotes spaced incorrectly")
+
 
 def make_name(t):
   """

commit 861feb66de401556c43dcb1e67fcd56e36727f35

murphi-format: fix mishandling of operators beginning with 0xe2
13e9b51b2b0b3d6338260229cb8a9aa115d2d294 introduced a bug wherein operators
beginning with the byte 0xe2 would incorrectly match the case for processing
strings opened with smart quotes. They would be discovered not to be a smart
quote and then emitted as a sequence of `TOKEN_UNKNOWN`s, one for each of their
bytes.

murphi-format/src/lex_get_token.c
@@ -142,7 +142,22 @@ int lex_get_token(lex_t *me, token_t *token) {
     }
     if (second != 0x80) {
       ungetc(second, me->src);
-      break;
+      {
+        bool have_op = false;
+        for (size_t i = 0; i < sizeof(OPERATORS) / sizeof(OPERATORS[0]); ++i)
+          have_op |= (unsigned char)OPERATORS[i][0] == 0xe2;
+        assert(have_op && "redundant handling; no operators start with 0xe2");
+        (void)have_op;
+      }
+      goto operator;
+    }
+    {
+      bool have_op = false;
+      for (size_t i = 0; i < sizeof(OPERATORS) / sizeof(OPERATORS[0]); ++i) {
+        const unsigned char *const op = (const unsigned char *)OPERATORS[i];
+        have_op |= op[0] == 0xe2 && op[1] == 0x80;
+      }
+      assert(!have_op && "mishandling operator beginning 0xe280");
     }
     ACCRUE(second);
     const int third = getc(me->src);
@@ -206,6 +221,7 @@ int lex_get_token(lex_t *me, token_t *token) {
   case '{':
     RET(TOKEN_OPEN_BRACE);
 
+  operator:
   default:
     if (may_be_operator(0, first)) {
       for (size_t i = 1;; ++i) {

tests/run-tests.py
@@ -1114,6 +1114,18 @@ class MurphiFormat(unittest.TestCase):
             "incorrect trailing space",
         )
 
+    def test_unicode_op(self):
+        """unicode operators starting with 0xe2 should be handled correctly"""
+
+        model = "rule x ∨ y ==> begin x := z; end"
+
+        ret, stdout, stderr = run(["murphi-format"], model)
+
+        self.assertEqual(ret, 0, "failed to reflow Murphi snippet")
+        self.assertEqual(stderr, "", "murphi-format printed errors/warnings")
+
+        self.assertIn(" x ∨ y ", stdout, "incorrect unicode operator handling")
+
 
 def make_name(t):
   """