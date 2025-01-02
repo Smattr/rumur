8eb6d7947c3099d89814214c542c9b56ab993ec0
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-01-02 17:15:01 +1100

    port murphi2murphi-unicode-div.py to Pytest

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -1615,3 +1615,23 @@ def test_murphi2murphi_switch_to_if2():
     # the generated model also should be valid syntax for Rumur
     ret, _, _ = run(["rumur", "--output", os.devnull], transformed)
     assert ret == 0
+
+
+def test_murphi2murphi_unicode_div():
+    """murphi2murphi’s ability to handle ÷"""
+
+    # model from the test directory involving a unicode division
+    model = Path(__file__).parent / "unicode-div.m"
+    assert model.exists()
+
+    # use the ASCII transformation to remove ÷
+    ret, transformed, _ = run(["murphi2murphi", "--to-ascii", model])
+    assert ret == 0
+
+    # the ÷ operator should have become /
+    assert re.search(r"\bx := 2 / x\b", transformed)
+    assert "÷" not in transformed
+
+    # the generated model also should be valid syntax for Rumur
+    ret, _, _ = run(["rumur", "--output", os.devnull], transformed)
+    assert ret == 0
