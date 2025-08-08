bfa4dcc158fac84f62e1a1baa59317a6d82cf3ca
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-08-08 17:05:50 +1000

    tune atomic accesses to liveness
    The aim here is to eek out slightly more performance on weak architectures like
    ARM. This is part of a larger series of changes to more precisely tune all our
    atomic accesses.

--- rumur/resources/header.c
+++ rumur/resources/header.c
@@ -3609,7 +3609,7 @@ static __attribute__((unused)) void mark_liveness(struct state *NONNULL s,
     /* If this state is shared (accessible by other threads) we need to operate
      * on its liveness data atomically.
      */
-    previous_value = __atomic_fetch_or(target, mask, __ATOMIC_SEQ_CST);
+    previous_value = __atomic_fetch_or(target, mask, __ATOMIC_ACQ_REL);
   } else {
     /* Otherwise we can use a cheaper ordinary OR. */
     previous_value = *target;
@@ -3642,7 +3642,7 @@ static unsigned long unknown_liveness(const struct state *NONNULL s) {
 
   for (size_t i = 0; i < sizeof(s->liveness) / sizeof(s->liveness[0]); i++) {
 
-    uintptr_t word = __atomic_load_n(&s->liveness[i], __ATOMIC_SEQ_CST);
+    uintptr_t word = __atomic_load_n(&s->liveness[i], __ATOMIC_ACQUIRE);
 
     for (size_t j = 0; j < sizeof(s->liveness[0]) * CHAR_BIT; j++) {
       if (i * sizeof(s->liveness[0]) * CHAR_BIT + j >= LIVENESS_COUNT) {
@@ -3678,8 +3678,8 @@ static unsigned long learn_liveness(struct state *NONNULL s,
   for (size_t i = 0; i < sizeof(s->liveness) / sizeof(s->liveness[0]); i++) {
 
     uintptr_t word_src =
-        __atomic_load_n(&successor->liveness[i], __ATOMIC_SEQ_CST);
-    uintptr_t word_dst = __atomic_load_n(&s->liveness[i], __ATOMIC_SEQ_CST);
+        __atomic_load_n(&successor->liveness[i], __ATOMIC_ACQUIRE);
+    uintptr_t word_dst = __atomic_load_n(&s->liveness[i], __ATOMIC_ACQUIRE);
 
     for (size_t j = 0; j < sizeof(s->liveness[0]) * CHAR_BIT; j++) {
       if (i * sizeof(s->liveness[0]) * CHAR_BIT + j >= LIVENESS_COUNT) {
