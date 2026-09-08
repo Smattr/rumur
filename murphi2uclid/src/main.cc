d98255a32cf957b0f9b0d8700741fd010cf9f376
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-09-07 17:04:38 -0700

    murphi2uclid: print nicer colourised error messages
    Github: #338 “location information in parsing rejections”

--- murphi2uclid/src/main.cc
+++ murphi2uclid/src/main.cc
@@ -18,6 +18,7 @@
 #include <sstream>
 #include <string>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <utility>
 #include <vector>
 
@@ -33,6 +34,9 @@ static std::shared_ptr<std::ostream> out;
 std::string module_name = "main";
 
 std::string numeric_type;
+
+/// use colour in error messages?
+static enum { AUTO, ON, OFF } color = AUTO;
 
 static bool is_valid_numeric_type(const char *s) {
   assert(s != NULL);
@@ -54,6 +58,8 @@ static void parse_args(int argc, char **argv) {
   for (;;) {
     static struct option options[] = {
         // clang-format off
+        { "color",        required_argument, 0, 129 },
+        { "colour",       required_argument, 0, 129 },
         { "help",         no_argument,       0, 'h' },
         { "module",       required_argument, 0, 'm' },
         { "numeric-type", required_argument, 0, 'n' },
@@ -109,6 +115,20 @@ static void parse_args(int argc, char **argv) {
       std::cout << "Murphi2Uclid version " << rumur_get_version() << '\n';
       exit(EXIT_SUCCESS);
 
+    case 129: // --color, --colour
+      if (strcmp(optarg, "auto") == 0) {
+        color = AUTO;
+      } else if (strcmp(optarg, "on") == 0) {
+        color = ON;
+      } else if (strcmp(optarg, "off") == 0) {
+        color = OFF;
+      } else {
+        std::cerr << "invalid --colour argument \"" << optarg << "\"\n"
+                  << "valid arguments are \"auto\", \"off\", and \"on\"\n";
+        exit(EXIT_FAILURE);
+      }
+      break;
+
     default:
       std::cerr << "unexpected error\n";
       exit(EXIT_FAILURE);
@@ -155,6 +175,78 @@ static dup_t make_stdin_dup() {
 
 static std::ostream &output() { return out == nullptr ? std::cout : *out; }
 
+static bool use_colors() {
+  if (color == AUTO)
+    color = isatty(STDERR_FILENO) ? ON : OFF;
+  return color == ON;
+}
+
+static const char *bold() {
+  if (use_colors())
+    return "\033[1m";
+  return "";
+}
+
+static const char *green() {
+  if (use_colors())
+    return "\033[32m";
+  return "";
+}
+
+static const char *red() {
+  if (use_colors())
+    return "\033[31m";
+  return "";
+}
+
+static const char *reset() {
+  if (use_colors())
+    return "\033[0m";
+  return "";
+}
+
+static const char *white() {
+  if (use_colors())
+    return "\033[37m";
+  return "";
+}
+
+static void print_location(std::istream &src, const rumur::location &location) {
+
+  // the type of position.line and position.column changes across Bison
+  // releases, so avoid some -Wsign-compare warnings by casting them in advance
+  auto loc_line = static_cast<unsigned long>(location.begin.line);
+  auto loc_col = static_cast<unsigned long>(location.begin.column);
+
+  std::string line;
+  unsigned long lineno = 0;
+  while (lineno < loc_line) {
+    if (!std::getline(src, line))
+      return;
+    lineno++;
+  }
+
+  // print the line, and construct an underline indicating the column location
+  std::ostringstream buf;
+  unsigned long col = 1;
+  for (const char &c : line) {
+    if (col == loc_col) {
+      buf << green() << bold() << '^' << reset();
+    } else if (col < loc_col) {
+      if (c == '\t') {
+        buf << '\t';
+      } else {
+        buf << ' ';
+      }
+    }
+    std::cerr << c;
+    col++;
+  }
+  std::cerr << '\n';
+
+  std::cerr << buf.str() << '\n';
+}
+
 int main(int argc, char **argv) {
 
   // parse command line options
@@ -170,7 +262,11 @@ int main(int argc, char **argv) {
   try {
     parsed = rumur::parse(*in.first);
   } catch (rumur::Error &e) {
-    std::cerr << e.loc << ":" << e.what() << '\n';
+    std::cerr << white() << bold() << in_filename << ':' << e.loc << ':'
+              << reset() << ' ' << red() << bold() << "error:" << reset() << ' '
+              << white() << bold() << e.what() << reset() << '\n';
+    in.first->seekg(0);
+    print_location(*in.first, e.loc);
     return EXIT_FAILURE;
   }
 
@@ -188,7 +284,11 @@ int main(int argc, char **argv) {
       resolve_symbols(*model);
       validate(*model);
     } catch (rumur::Error &e) {
-      std::cerr << e.loc << ":" << e.what() << '\n';
+      std::cerr << white() << bold() << in_filename << ':' << e.loc << ':'
+                << reset() << ' ' << red() << bold() << "error:" << reset()
+                << ' ' << white() << bold() << e.what() << reset() << '\n';
+      in.first->seekg(0);
+      print_location(*in.first, e.loc);
       return EXIT_FAILURE;
     }
   }
@@ -200,7 +300,11 @@ int main(int argc, char **argv) {
   try {
     check(*parsed);
   } catch (rumur::Error &e) {
-    std::cerr << e.loc << ":" << e.what() << '\n';
+    std::cerr << white() << bold() << in_filename << ':' << e.loc << ':'
+              << reset() << ' ' << red() << bold() << "error:" << reset() << ' '
+              << white() << bold() << e.what() << reset() << '\n';
+    in.first->seekg(0);
+    print_location(*in.first, e.loc);
     return EXIT_FAILURE;
   }
 
