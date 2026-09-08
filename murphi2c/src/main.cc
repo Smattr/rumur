106cb678ec6da9033c1582e98621f6dce036a9f0
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-09-07 17:04:38 -0700

    murphi2c: remove an unnecessary use of 'std::string'

--- murphi2c/src/main.cc
+++ murphi2c/src/main.cc
@@ -21,7 +21,7 @@
 #include <unistd.h>
 #include <vector>
 
-static std::string in_filename = "<stdin>";
+static const char *in_filename = "<stdin>";
 static std::shared_ptr<std::istream> in;
 static std::shared_ptr<std::ostream> out;
 
